45b360876ae112f5ce721d90da5f9e6da36e7881
E Kawashima <e-kwsm@users.noreply.github.com>
2019-05-20 06:08:12 +0900

    W605

--- rdkit/Chem/Pharm3D/Pharmacophore.py
+++ rdkit/Chem/Pharm3D/Pharmacophore.py
@@ -177,7 +177,7 @@ class ExplicitPharmacophore:
 
   def initFromLines(self, lines):
     import re
-    spaces = re.compile('[\ \t]+')
+    spaces = re.compile(r'[\ \t]+')
 
     feats = []
     rads = []
