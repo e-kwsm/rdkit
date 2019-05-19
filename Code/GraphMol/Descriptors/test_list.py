270ca459e1cff70b2705a77470e3e6526b5daeb0
E Kawashima <e-kwsm@users.noreply.github.com>
2019-05-20 07:06:16 +0900

    F811

--- Code/GraphMol/Descriptors/test_list.py
+++ Code/GraphMol/Descriptors/test_list.py
@@ -5,7 +5,6 @@ tests = [("testExecs/testDescriptors.exe", "", {}), ]
 longTests = []
 
 if __name__ == '__main__':
-  import sys
   from rdkit import TestRunner
   failed, tests = TestRunner.RunScript('test_list.py', 0, 1)
   sys.exit(len(failed))
