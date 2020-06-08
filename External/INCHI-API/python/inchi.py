aebb19f05f7f7de174f44e3883aa64462931b6e2
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2020-06-08 12:31:53 +0900

    External/INCHI-API/python/inchi.py

--- External/INCHI-API/python/inchi.py
+++ External/INCHI-API/python/inchi.py
@@ -53,19 +53,19 @@ class InchiReadWriteError(Exception):
 def MolFromInchi(inchi, sanitize=True, removeHs=True, logLevel=None, treatWarningAsError=False):
   """Construct a molecule from a InChI string
 
-    Keyword arguments:
-    sanitize -- set to True to enable sanitization of the molecule. Default is
-    True
-    removeHs -- set to True to remove Hydrogens from a molecule. This only
-    makes sense when sanitization is enabled
-    logLevel -- the log level used for logging logs and messages from InChI
-    API. set to None to diable the logging completely
-    treatWarningAsError -- set to True to raise an exception in case of a
-    molecule that generates warning in calling InChI API. The resultant
-    molecule  and error message are part of the excpetion
+    Arguments:
+      sanitize: set to True to enable sanitization of the molecule. Default is
+        True
+      removeHs: set to True to remove Hydrogens from a molecule. This only
+        makes sense when sanitization is enabled
+      logLevel: the log level used for logging logs and messages from InChI
+        API. set to None to diable the logging completely
+      treatWarningAsError: set to True to raise an exception in case of a
+        molecule that generates warning in calling InChI API. The resultant
+        molecule  and error message are part of the excpetion
 
     Returns:
-    a rdkit.Chem.rdchem.Mol instance
+      a rdkit.Chem.rdchem.Mol instance
     """
   try:
     mol, retcode, message, log = rdinchi.InchiToMol(inchi, sanitize, removeHs)
@@ -93,17 +93,17 @@ def MolFromInchi(inchi, sanitize=True, removeHs=True, logLevel=None, treatWarnin
 def MolToInchiAndAuxInfo(mol, options="", logLevel=None, treatWarningAsError=False):
   """Returns the standard InChI string and InChI auxInfo for a molecule
 
-    Keyword arguments:
-    logLevel -- the log level used for logging logs and messages from InChI
-    API. set to None to diable the logging completely
-    treatWarningAsError -- set to True to raise an exception in case of a
-    molecule that generates warning in calling InChI API. The resultant InChI
-    string and AuxInfo string as well as the error message are encoded in the
-    exception.
+    Arguments:
+      logLevel: the log level used for logging logs and messages from InChI
+        API. set to None to diable the logging completely
+      treatWarningAsError: set to True to raise an exception in case of a
+        molecule that generates warning in calling InChI API. The resultant InChI
+        string and AuxInfo string as well as the error message are encoded in the
+        exception.
 
     Returns:
-    a tuple of the standard InChI string and the auxInfo string returned by
-    InChI API, in that order, for the input molecule
+      a tuple of the standard InChI string and the auxInfo string returned by
+      InChI API, in that order, for the input molecule
     """
   inchi, retcode, message, logs, aux = rdinchi.MolToInchi(mol, options)
   if logLevel is not None:
@@ -125,17 +125,17 @@ def MolToInchiAndAuxInfo(mol, options="", logLevel=None, treatWarningAsError=Fal
 def MolBlockToInchiAndAuxInfo(molblock, options="", logLevel=None, treatWarningAsError=False):
   """Returns the standard InChI string and InChI auxInfo for a mol block
 
-    Keyword arguments:
-    logLevel -- the log level used for logging logs and messages from InChI
-    API. set to None to diable the logging completely
-    treatWarningAsError -- set to True to raise an exception in case of a
-    molecule that generates warning in calling InChI API. The resultant InChI
-    string and AuxInfo string as well as the error message are encoded in the
-    exception.
+    Arguments:
+      logLevel: the log level used for logging logs and messages from InChI
+        API. set to None to diable the logging completely
+      treatWarningAsError: set to True to raise an exception in case of a
+        molecule that generates warning in calling InChI API. The resultant InChI
+        string and AuxInfo string as well as the error message are encoded in the
+        exception.
 
     Returns:
-    a tuple of the standard InChI string and the auxInfo string returned by
-    InChI API, in that order, for the input molecule
+      a tuple of the standard InChI string and the auxInfo string returned by
+      InChI API, in that order, for the input molecule
     """
   inchi, retcode, message, logs, aux = rdinchi.MolBlockToInchi(molblock, options)
   if logLevel is not None:
@@ -157,16 +157,16 @@ def MolBlockToInchiAndAuxInfo(molblock, options="", logLevel=None, treatWarningA
 def MolToInchi(mol, options="", logLevel=None, treatWarningAsError=False):
   """Returns the standard InChI string for a molecule
 
-    Keyword arguments:
-    logLevel -- the log level used for logging logs and messages from InChI
-    API. set to None to diable the logging completely
-    treatWarningAsError -- set to True to raise an exception in case of a
-    molecule that generates warning in calling InChI API. The resultant InChI
-    string and AuxInfo string as well as the error message are encoded in the
-    exception.
+    Arguments:
+      logLevel: the log level used for logging logs and messages from InChI
+        API. set to None to diable the logging completely
+      treatWarningAsError: set to True to raise an exception in case of a
+        molecule that generates warning in calling InChI API. The resultant InChI
+        string and AuxInfo string as well as the error message are encoded in the
+        exception.
 
     Returns:
-    the standard InChI string returned by InChI API for the input molecule
+      the standard InChI string returned by InChI API for the input molecule
     """
   if options.find('AuxNone') == -1:
     if options:
@@ -185,16 +185,16 @@ def MolToInchi(mol, options="", logLevel=None, treatWarningAsError=False):
 def MolBlockToInchi(molblock, options="", logLevel=None, treatWarningAsError=False):
   """Returns the standard InChI string for a mol block
 
-    Keyword arguments:
-    logLevel -- the log level used for logging logs and messages from InChI
-    API. set to None to diable the logging completely
-    treatWarningAsError -- set to True to raise an exception in case of a
-    molecule that generates warning in calling InChI API. The resultant InChI
-    string and AuxInfo string as well as the error message are encoded in the
-    exception.
+    Arguments:
+      logLevel: the log level used for logging logs and messages from InChI
+        API. set to None to diable the logging completely
+      treatWarningAsError: set to True to raise an exception in case of a
+        molecule that generates warning in calling InChI API. The resultant InChI
+        string and AuxInfo string as well as the error message are encoded in the
+        exception.
 
     Returns:
-    the standard InChI string returned by InChI API for the input molecule
+      the standard InChI string returned by InChI API for the input molecule
     """
   if options.find('AuxNone') == -1:
     if options:
@@ -222,7 +222,7 @@ def MolToInchiKey(mol, options=""):
   """Returns the standard InChI key for a molecule
 
     Returns:
-    the standard InChI key returned by InChI API for the input molecule
+      the standard InChI key returned by InChI API for the input molecule
     """
   return rdinchi.MolToInchiKey(mol,options)
 
