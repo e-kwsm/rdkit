1c83195890bc4ac2eda9c23d7014bd857407d9b9
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2021-06-12 05:25:41 +0900

    modernize-loop-convert

--- Code/GraphMol/molopstest.cpp
+++ Code/GraphMol/molopstest.cpp
@@ -4134,9 +4134,8 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
-      mmap[mvit->second] = mvit->first;
+    for (const auto &mvit : mv) {
+      mmap[mvit.second] = mvit.first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[2], mmap[3])->getBondType() ==
                 Bond::DOUBLE);
@@ -4171,9 +4170,8 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
-      mmap[mvit->second] = mvit->first;
+    for (const auto &mvit : mv) {
+      mmap[mvit.second] = mvit.first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[10], mmap[11])->getBondType() ==
                 Bond::DOUBLE);
@@ -4301,9 +4299,8 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
-      mmap[mvit->second] = mvit->first;
+    for (const auto &mvit : mv) {
+      mmap[mvit.second] = mvit.first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[1], mmap[2])->getBondType() ==
                 Bond::DOUBLE);
@@ -4330,9 +4327,8 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
-      mmap[mvit->second] = mvit->first;
+    for (const auto &mvit : mv) {
+      mmap[mvit.second] = mvit.first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[4], mmap[5])->getBondType() ==
                 Bond::DOUBLE);
@@ -4377,9 +4373,8 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
-      mmap[mvit->second] = mvit->first;
+    for (const auto &mvit : mv) {
+      mmap[mvit.second] = mvit.first;
     }
 
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[1], mmap[2])->getBondType() ==
@@ -4449,9 +4444,8 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
-      mmap[mvit->second] = mvit->first;
+    for (const auto &mvit : mv) {
+      mmap[mvit.second] = mvit.first;
     }
 
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[21], mmap[13])->getBondType() ==
@@ -4475,9 +4469,8 @@ void testBasicCanon() {
     m2 = SmilesToMol(tsmi);
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     mmap.clear();
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
-      mmap[mvit->second] = mvit->first;
+    for (const auto &mvit : mv) {
+      mmap[mvit.second] = mvit.first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[21], mmap[13])->getBondType() ==
                 Bond::DOUBLE);
@@ -4514,9 +4507,8 @@ void testBasicCanon() {
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
     mmap.clear();
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
-      mmap[mvit->second] = mvit->first;
+    for (const auto &mvit : mv) {
+      mmap[mvit.second] = mvit.first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[1], mmap[2])->getBondType() ==
                 Bond::DOUBLE);
@@ -5604,9 +5596,8 @@ void testGithubIssue539() {
     smilesVec.emplace_back("C1=CC=C[CH+]C=C1");
     smilesVec.emplace_back("c1c[cH+]1");
     smilesVec.emplace_back("c1ccc[cH+]cc1");
-    for (std::vector<std::string>::const_iterator smiles = smilesVec.begin();
-         smiles != smilesVec.end(); ++smiles) {
-      RWMol *m = SmilesToMol(*smiles);
+    for (const auto &smiles : smilesVec) {
+      RWMol *m = SmilesToMol(smiles);
       TEST_ASSERT(m);
       bool allConjugated = true;
       for (unsigned int i = 0; allConjugated && i < m->getNumBonds(); ++i) {
