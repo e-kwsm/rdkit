7669c7aab321f7e1858d3ec5127c86d8cd000472
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2021-11-28 21:52:52 +0900

    modernize-use-using

--- Code/RDBoost/Wrap.h
+++ Code/RDBoost/Wrap.h
@@ -290,11 +290,11 @@ struct iterable_converter {
 
     // Obtain a handle to the memory block that the converter has allocated
     // for the C++ type.
-    typedef python::converter::rvalue_from_python_storage<Container>
-        storage_type;
+    using storage_type =
+        python::converter::rvalue_from_python_storage<Container>;
     void *storage = reinterpret_cast<storage_type *>(data)->storage.bytes;
 
-    typedef python::stl_input_iterator<typename Container::value_type> iterator;
+    using iterator = python::stl_input_iterator<typename Container::value_type>;
 
     // Allocate the C++ type into the converter's memory block, and assign
     // its handle to the converter's convertible variable.  The C++
