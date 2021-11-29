09e713e0776686bdba033d5ec60691fad0583aa3
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2021-11-29 17:50:23 +0900

    refactor: modernize-use-equals-delete

--- Code/GraphMol/Depictor/Templates.h
+++ Code/GraphMol/Depictor/Templates.h
@@ -21,6 +21,8 @@
 namespace RDDepict {
 class RDKIT_DEPICTOR_EXPORT CoordinateTemplates {
  public:
+  CoordinateTemplates(const CoordinateTemplates &) = delete;
+  CoordinateTemplates &operator=(const CoordinateTemplates &) = delete;
   //! returns a reference to the singleton CoordinateTemplates
   /*
       \return a reference to the singleton CoordinateTemplates
@@ -76,8 +78,6 @@ class RDKIT_DEPICTOR_EXPORT CoordinateTemplates {
 
  private:
   CoordinateTemplates() { loadDefaultTemplates(); }
-  CoordinateTemplates(const CoordinateTemplates &) = delete;
-  CoordinateTemplates &operator=(const CoordinateTemplates &) = delete;
 
   void clearTemplates() {
     for (auto &[atom_cout, romols] : m_templates) {
