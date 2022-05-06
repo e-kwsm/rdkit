b76c85c6e81f863c73bd44695d63342511cfacfa
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2022-05-07 05:00:33 +0900

    Code/GraphMol/Aromaticity.cpp Code/GraphMol/Chirality.cpp Code/GraphMol/Conformer.h Code/GraphMol/FileParsers/FileParserUtils.h Code/GraphMol/ROMol.cpp Code/GraphMol/ROMol.h Code/GraphMol/RWMol.cpp Code/GraphMol/molopstest.cpp Code/RDGeneral/catch_logs.cpp

--- Code/GraphMol/RWMol.cpp
+++ Code/GraphMol/RWMol.cpp
@@ -394,7 +394,7 @@ void RWMol::removeAtom(Atom *atom, bool clearProps) {
   }
 
   // do the same with the coordinates in the conformations
-  for (auto conf : d_confs) {
+  for (const auto &conf : d_confs) {
     RDGeom::POINT3D_VECT &positions = conf->getPositions();
     auto pi = positions.begin();
     for (unsigned int i = 0; i < getNumAtoms() - 1; i++) {
