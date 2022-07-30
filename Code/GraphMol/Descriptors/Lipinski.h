f2c7b5ec7b15ea3467c522ba44a5c75a98a71534
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2022-07-30 23:39:08 +0900

    Code/GraphMol/ChemReactions/ReactionFingerprints.h Code/GraphMol/Chirality.h Code/GraphMol/Depictor/EmbeddedFrag.h Code/GraphMol/Depictor/RDDepictor.cpp Code/GraphMol/Descriptors/BCUT.h Code/GraphMol/Descriptors/Lipinski.h

--- Code/GraphMol/Descriptors/Lipinski.h
+++ Code/GraphMol/Descriptors/Lipinski.h
@@ -43,7 +43,8 @@ RDKIT_DESCRIPTORS_EXPORT extern const std::string NumRotatableBondsVersion;
 //! calculates the number of rotatable bonds
 /*!
   \param mol           the molecule of interest
-  \param strict        if Strict, a stricter definition of rotable bonds is used
+  \param useStrictDefinition
+                       if Strict, a stricter definition of rotable bonds is used
                            this excludes amides, esters, etc.
                        if StrictLinkages, a much stricter definition that
                            handles rotatable bonds between rings as well.
