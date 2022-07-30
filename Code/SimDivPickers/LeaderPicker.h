a6a52827761732d8922cec31bdefeed4b40f2391
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2022-07-30 23:50:53 +0900

    Code/SimDivPickers/LeaderPicker.h Code/SimDivPickers/MaxMinPicker.h

--- Code/SimDivPickers/LeaderPicker.h
+++ Code/SimDivPickers/LeaderPicker.h
@@ -58,9 +58,6 @@ class LeaderPicker : public DistPicker {
    *   \param pickSize - the number items to pick from pool (<= poolSize)
    *
    *   \param firstPicks - (optional)the first items in the pick list
-   *
-   *   \param seed - (optional) seed for the random number generator. If this is
-   *<0 the generator will be seeded with a random number.
    */
   template <typename T>
   RDKit::INT_VECT lazyPick(T &func, unsigned int poolSize,
