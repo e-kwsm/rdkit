50fdd8fcd053fa7a20184c9094830c331023e9ac
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2022-09-05 23:19:26 +0900

    Code/GraphMol/ChemTransforms/MolFragmenter.cpp Code/GraphMol/ChemTransforms/testChemTransforms.cpp Code/GraphMol/Descriptors/DCLV.cpp Code/GraphMol/FileParsers/FileParsers.h Code/GraphMol/FileParsers/ForwardSDMolSupplier.cpp Code/GraphMol/FileParsers/MaeWriter.cpp Code/GraphMol/FileParsers/MolFileStereochem.cpp Code/GraphMol/FileParsers/MultithreadedMolSupplier.cpp Code/GraphMol/FileParsers/test1.cpp Code/GraphMol/FileParsers/testMolWriter.cpp Code/GraphMol/Fingerprints/MorganGenerator.h Code/GraphMol/Fingerprints/TopologicalTorsionGenerator.cpp Code/GraphMol/MarvinParse/MarvinDefs.cpp Code/GraphMol/MarvinParse/MarvinWriter.cpp Code/GraphMol/MolBundle.h Code/GraphMol/MolChemicalFeatures/testFeatures.cpp Code/GraphMol/MolDraw2D/DrawMol.cpp Code/GraphMol/MolDraw2D/DrawMolMCHLasso.cpp Code/GraphMol/MolDraw2D/MolDraw2DHelpers.h Code/GraphMol/MolDraw2D/MolDraw2DUtils.cpp Code/GraphMol/MolDraw2D/test1.cpp Code/GraphMol/MolOps.cpp Code/GraphMol/MolStandardize/Charge.cpp Code/GraphMol/MolStandardize/MolStandardize.h Code/GraphMol/MolStandardize/catch_tests.cpp Code/GraphMol/ROMol.cpp Code/GraphMol/SmilesParse/SmartsWrite.h Code/GraphMol/SmilesParse/SmilesParse.cpp Code/GraphMol/SmilesParse/SmilesParseOps.h Code/GraphMol/SmilesParse/SmilesWrite.h Code/GraphMol/SmilesParse/cxsmiles_test.cpp Code/GraphMol/SmilesParse/smatest.cpp Code/GraphMol/StereoGroup.cpp Code/GraphMol/Wrap/Mol.cpp Code/GraphMol/new_canon.h Code/MinimalLib/cffiwrapper.cpp External/INCHI-API/inchi.h

--- Code/GraphMol/FileParsers/MaeWriter.cpp
+++ Code/GraphMol/FileParsers/MaeWriter.cpp
@@ -549,7 +549,7 @@ MaeWriter::MaeWriter(std::shared_ptr<std::ostream> outStream)
   }
 }
 
-MaeWriter::~MaeWriter() { close(); };
+MaeWriter::~MaeWriter() { close(); }
 
 void MaeWriter::open() { dp_writer.reset(new mae::Writer(dp_ostream)); }
 
