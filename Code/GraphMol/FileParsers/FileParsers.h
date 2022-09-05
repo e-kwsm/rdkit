1a267a28f965262a81e943b910ae9b949f6b3c49
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2022-09-05 23:19:26 +0900

    Code/GraphMol/ChemTransforms/MolFragmenter.cpp Code/GraphMol/ChemTransforms/testChemTransforms.cpp Code/GraphMol/Descriptors/DCLV.cpp Code/GraphMol/FileParsers/FileParsers.h Code/GraphMol/FileParsers/ForwardSDMolSupplier.cpp Code/GraphMol/FileParsers/MaeWriter.cpp Code/GraphMol/FileParsers/MolFileStereochem.cpp Code/GraphMol/FileParsers/MultithreadedMolSupplier.cpp Code/GraphMol/FileParsers/test1.cpp Code/GraphMol/FileParsers/testMolWriter.cpp Code/GraphMol/Fingerprints/MorganGenerator.h Code/GraphMol/Fingerprints/TopologicalTorsionGenerator.cpp Code/GraphMol/MarvinParse/MarvinDefs.cpp Code/GraphMol/MarvinParse/MarvinWriter.cpp Code/GraphMol/MolBundle.h Code/GraphMol/MolChemicalFeatures/testFeatures.cpp Code/GraphMol/MolDraw2D/DrawMol.cpp Code/GraphMol/MolDraw2D/DrawMolMCHLasso.cpp Code/GraphMol/MolDraw2D/MolDraw2DHelpers.h Code/GraphMol/MolDraw2D/MolDraw2DUtils.cpp Code/GraphMol/MolDraw2D/test1.cpp Code/GraphMol/MolOps.cpp Code/GraphMol/MolStandardize/Charge.cpp Code/GraphMol/MolStandardize/MolStandardize.h Code/GraphMol/MolStandardize/catch_tests.cpp Code/GraphMol/ROMol.cpp Code/GraphMol/SmilesParse/SmartsWrite.h Code/GraphMol/SmilesParse/SmilesParse.cpp Code/GraphMol/SmilesParse/SmilesParseOps.h Code/GraphMol/SmilesParse/SmilesWrite.h Code/GraphMol/SmilesParse/cxsmiles_test.cpp Code/GraphMol/SmilesParse/smatest.cpp Code/GraphMol/StereoGroup.cpp Code/GraphMol/Wrap/Mol.cpp Code/GraphMol/new_canon.h Code/MinimalLib/cffiwrapper.cpp External/INCHI-API/inchi.h

--- Code/GraphMol/FileParsers/FileParsers.h
+++ Code/GraphMol/FileParsers/FileParsers.h
@@ -92,13 +92,13 @@ inline RWMol *MolDataStreamToMol(std::istream *inStream, unsigned int &line,
   ps.removeHs = removeHs;
   ps.strictParsing = strictParsing;
   return v2::FileParsers::MolFromMolDataStream(*inStream, line, ps).release();
-};
+}
 // \overload
 inline RWMol *MolDataStreamToMol(std::istream &inStream, unsigned int &line,
                                  bool sanitize = true, bool removeHs = true,
                                  bool strictParsing = true) {
   return MolDataStreamToMol(&inStream, line, sanitize, removeHs, strictParsing);
-};
+}
 // \brief construct a molecule from an MDL mol block
 /*!
  *   \param molBlock - string containing the mol block
@@ -116,7 +116,7 @@ inline RWMol *MolBlockToMol(const std::string &molBlock, bool sanitize = true,
   ps.removeHs = removeHs;
   ps.strictParsing = strictParsing;
   return v2::FileParsers::MolFromMolBlock(molBlock, ps).release();
-};
+}
 
 // \brief construct a molecule from an MDL mol file
 /*!
@@ -135,7 +135,7 @@ inline RWMol *MolFileToMol(const std::string &fName, bool sanitize = true,
   ps.removeHs = removeHs;
   ps.strictParsing = strictParsing;
   return v2::FileParsers::MolFromMolFile(fName, ps).release();
-};
+}
 }  // namespace v1
 
 //-----
