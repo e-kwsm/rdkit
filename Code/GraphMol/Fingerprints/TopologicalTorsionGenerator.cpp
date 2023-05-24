275e54b603268c6a4cdf9e8a30691fc8f179c4e7
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2023-05-25 03:34:49 +0900

    Code/GraphMol/Fingerprints/TopologicalTorsionGenerator.cpp

--- Code/GraphMol/Fingerprints/TopologicalTorsionGenerator.cpp
+++ Code/GraphMol/Fingerprints/TopologicalTorsionGenerator.cpp
@@ -114,13 +114,12 @@ TopologicalTorsionEnvGenerator<OutputType>::getEnvironments(
   PATH_LIST paths = findAllPathsOfLengthN(
       mol, topologicalTorsionArguments->d_torsionAtomCount, useBonds, useHs,
       rootedAtAtom, topologicalTorsionArguments->df_onlyShortestPaths);
-  for (auto pathIt = paths.begin(); pathIt != paths.end(); ++pathIt) {
+  for (auto &path : paths) {
     bool keepIt = true;
     if (fromAtomsBV) {
       keepIt = false;
     }
     std::vector<std::uint32_t> pathCodes;
-    const PATH_TYPE &path = *pathIt;
     if (fromAtomsBV) {
       if (fromAtomsBV->test(static_cast<std::uint32_t>(path.front())) ||
           fromAtomsBV->test(static_cast<std::uint32_t>(path.back()))) {
