3a6d9413c16d0289c0828db45dda77eceb4960da
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2023-05-25 04:12:23 +0900

    Code/GraphMol/MolHash/catch_tests.cpp

--- Code/GraphMol/MolHash/catch_tests.cpp
+++ Code/GraphMol/MolHash/catch_tests.cpp
@@ -472,9 +472,9 @@ TEST_CASE("tautomer v2") {
             MolHash::MolHash(&cp, MolHash::HashFunction::HetAtomTautomerv2);
         CHECK(hsh == ref);
       }
-      for (auto i = 0u; i < diff.size(); ++i) {
-        INFO(same[0] + "->" + diff[i]);
-        std::unique_ptr<RWMol> m2{SmilesToMol(diff[i])};
+      for (const auto &i : diff) {
+        INFO(same[0] + "->" + i);
+        std::unique_ptr<RWMol> m2{SmilesToMol(i)};
         REQUIRE(m2);
         RWMol cp(*m2);
         auto hsh =
