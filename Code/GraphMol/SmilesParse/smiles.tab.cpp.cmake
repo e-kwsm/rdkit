54ca60288d69ad4a697843044211af1557925a42
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2023-05-26 20:35:37 +0900

    style: apply readability-braces-around-statements

--- Code/GraphMol/SmilesParse/smiles.tab.cpp.cmake
+++ Code/GraphMol/SmilesParse/smiles.tab.cpp.cmake
@@ -541,8 +541,9 @@ union yyalloc
       do                                        \
         {                                       \
           YYPTRDIFF_T yyi;                      \
-          for (yyi = 0; yyi < (Count); yyi++)   \
+          for (yyi = 0; yyi < (Count); yyi++) { \
             (Dst)[yyi] = (Src)[yyi];            \
+          }                                     \
         }                                       \
       while (0)
 #  endif
@@ -847,8 +848,9 @@ enum { YYENOMEM = -2 };
 
 # define YYDPRINTF(Args)                        \
 do {                                            \
-  if (yydebug)                                  \
+  if (yydebug) {                                \
     YYFPRINTF Args;                             \
+  }                                             \
 } while (0)
 
 
@@ -886,8 +888,9 @@ yy_symbol_value_print (FILE *yyo,
   YY_USE (scanner);
   YY_USE (start_token);
   YY_USE (current_token_position);
-  if (!yyvaluep)
+  if (!yyvaluep) {
     return;
+  }
   YY_IGNORE_MAYBE_UNINITIALIZED_BEGIN
   YY_USE (yykind);
   YY_IGNORE_MAYBE_UNINITIALIZED_END
@@ -928,8 +931,9 @@ yy_stack_print (yy_state_t *yybottom, yy_state_t *yytop)
 
 # define YY_STACK_PRINT(Bottom, Top)                            \
 do {                                                            \
-  if (yydebug)                                                  \
+  if (yydebug) {                                                \
     yy_stack_print ((Bottom), (Top));                           \
+  }                                                             \
 } while (0)
 
 
@@ -959,8 +963,9 @@ yy_reduce_print (yy_state_t *yyssp, YYSTYPE *yyvsp,
 
 # define YY_REDUCE_PRINT(Rule)          \
 do {                                    \
-  if (yydebug)                          \
+  if (yydebug) {                        \
     yy_reduce_print (yyssp, yyvsp, Rule, input, molList, lastAtom, lastBond, numAtomsParsed, numBondsParsed, branchPoints, scanner, start_token, current_token_position); \
+  }                                     \
 } while (0)
 
 /* Nonzero means print parse trace.  It is left uninitialized so that
@@ -1014,8 +1019,9 @@ yydestruct (const char *yymsg,
   YY_USE (scanner);
   YY_USE (start_token);
   YY_USE (current_token_position);
-  if (!yymsg)
+  if (!yymsg) {
     yymsg = "Deleting";
+  }
   YY_SYMBOL_PRINT (yymsg, yykind, yyvaluep, yylocationp);
 
   YY_IGNORE_MAYBE_UNINITIALIZED_BEGIN
@@ -1189,24 +1195,28 @@ yysetstate:
       }
 # else /* defined YYSTACK_RELOCATE */
       /* Extend the stack our own way.  */
-      if (YYMAXDEPTH <= yystacksize)
+      if (YYMAXDEPTH <= yystacksize) {
         YYNOMEM;
+      }
       yystacksize *= 2;
-      if (YYMAXDEPTH < yystacksize)
+      if (YYMAXDEPTH < yystacksize) {
         yystacksize = YYMAXDEPTH;
+      }
 
       {
         yy_state_t *yyss1 = yyss;
         union yyalloc *yyptr =
           YY_CAST (union yyalloc *,
                    YYSTACK_ALLOC (YY_CAST (YYSIZE_T, YYSTACK_BYTES (yystacksize))));
-        if (! yyptr)
+        if (! yyptr) {
           YYNOMEM;
+        }
         YYSTACK_RELOCATE (yyss_alloc, yyss);
         YYSTACK_RELOCATE (yyvs_alloc, yyvs);
 #  undef YYSTACK_RELOCATE
-        if (yyss1 != yyssa)
+        if (yyss1 != yyssa) {
           YYSTACK_FREE (yyss1);
+        }
       }
 # endif
 
@@ -1218,14 +1228,16 @@ yysetstate:
                   YY_CAST (long, yystacksize)));
       YY_IGNORE_USELESS_CAST_END
 
-      if (yyss + yystacksize - 1 <= yyssp)
+      if (yyss + yystacksize - 1 <= yyssp) {
         YYABORT;
+      }
     }
 #endif /* !defined yyoverflow && !defined YYSTACK_RELOCATE */
 
 
-  if (yystate == YYFINAL)
+  if (yystate == YYFINAL) {
     YYACCEPT;
+  }
 
   goto yybackup;
 
@@ -1239,8 +1251,9 @@ yybackup:
 
   /* First try to decide what to do without reference to lookahead token.  */
   yyn = yypact[yystate];
-  if (yypact_value_is_default (yyn))
+  if (yypact_value_is_default (yyn)) {
     goto yydefault;
+  }
 
   /* Not known => get a lookahead token if don't already have one.  */
 
@@ -1276,21 +1289,24 @@ yybackup:
   /* If the proper action on seeing token YYTOKEN is to reduce or to
      detect an error, take that action.  */
   yyn += yytoken;
-  if (yyn < 0 || YYLAST < yyn || yycheck[yyn] != yytoken)
+  if (yyn < 0 || YYLAST < yyn || yycheck[yyn] != yytoken) {
     goto yydefault;
+  }
   yyn = yytable[yyn];
   if (yyn <= 0)
     {
-      if (yytable_value_is_error (yyn))
+      if (yytable_value_is_error (yyn)) {
         goto yyerrlab;
+      }
       yyn = -yyn;
       goto yyreduce;
     }
 
   /* Count tokens shifted since error; after three, turn off error
      status.  */
-  if (yyerrstatus)
+  if (yyerrstatus) {
     yyerrstatus--;
+  }
 
   /* Shift the lookahead token.  */
   YY_SYMBOL_PRINT ("Shifting", yytoken, &yylval, &yylloc);
@@ -1309,8 +1325,9 @@ yybackup:
 `-----------------------------------------------------------*/
 yydefault:
   yyn = yydefact[yystate];
-  if (yyn == 0)
+  if (yyn == 0) {
     goto yyerrlab;
+  }
   goto yyreduce;
 
 
@@ -1828,8 +1845,9 @@ yyerrlab:
       if (yychar <= YYEOF)
         {
           /* Return failure if at end of input.  */
-          if (yychar == YYEOF)
+          if (yychar == YYEOF) {
             YYABORT;
+          }
         }
       else
         {
@@ -1850,8 +1868,9 @@ yyerrlab:
 yyerrorlab:
   /* Pacify compilers when the user code never invokes YYERROR and the
      label yyerrorlab therefore never appears in user code.  */
-  if (0)
+  if (0) {
     YYERROR;
+  }
   ++yynerrs;
 
   /* Do not reclaim the symbols of the rule whose action triggered
@@ -1879,14 +1898,16 @@ yyerrlab1:
           if (0 <= yyn && yyn <= YYLAST && yycheck[yyn] == YYSYMBOL_YYerror)
             {
               yyn = yytable[yyn];
-              if (0 < yyn)
+              if (0 < yyn) {
                 break;
+              }
             }
         }
 
       /* Pop the current state because it cannot handle the error token.  */
-      if (yyssp == yyss)
+      if (yyssp == yyss) {
         YYABORT;
+      }
 
 
       yydestruct ("Error: popping",
@@ -1956,8 +1977,9 @@ yyreturnlab:
       YYPOPSTACK (1);
     }
 #ifndef yyoverflow
-  if (yyss != yyssa)
+  if (yyss != yyssa) {
     YYSTACK_FREE (yyss);
+  }
 #endif
 
   return yyresult;
