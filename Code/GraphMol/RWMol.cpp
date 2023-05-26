25a895cef53c3d8ef4e9ed40c9b218a3dabfd574
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2023-05-26 20:35:37 +0900

    style: apply readability-braces-around-statements

--- Code/GraphMol/RWMol.cpp
+++ Code/GraphMol/RWMol.cpp
@@ -682,7 +682,7 @@ void RWMol::batchRemoveBonds() {
     if (!delBonds[i - 1]) {
       continue;
     }
-    unsigned int idx = rdcast<unsigned int>(i - 1);
+    auto idx = rdcast<unsigned int>(i - 1);
     Bond *bnd = getBondWithIdx(idx);
     if (!bnd) {
       continue;
