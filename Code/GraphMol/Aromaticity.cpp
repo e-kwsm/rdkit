ec47ff5f5e0ba2f8a4f5cd47cc522ee5a178f22c
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2023-06-18 21:10:23 +0900

    Code/

--- Code/GraphMol/Aromaticity.cpp
+++ Code/GraphMol/Aromaticity.cpp
@@ -969,9 +969,9 @@ void setMMFFAromaticity(RWMol &mol) {
           atom = mol.getAtomWithIdx(atomRings[i][j]);
           // if not, move on
           if ((atom->getAtomicNum() != 6) &&
-              (!((atom->getAtomicNum() == 7) &&
-                 ((atom->getExplicitValence() + atom->getNumImplicitHs()) ==
-                  4)))) {
+              ((atom->getAtomicNum() != 7) ||
+               ((atom->getExplicitValence() + atom->getNumImplicitHs()) !=
+                4))) {
             continue;
           }
           // loop over neighbors
