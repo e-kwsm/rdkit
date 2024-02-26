6d974ea0d28125696f6b482dfccc4197f9e473a6
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-02-26 10:38:02 +0900

    rdkit/Chem/AtomPairs/Pairs.py

--- rdkit/Chem/AtomPairs/Pairs.py
+++ rdkit/Chem/AtomPairs/Pairs.py
@@ -43,15 +43,15 @@ def pyScorePair(at1, at2, dist, atomCodes=None, includeChirality=False):
   >>> c1 = Utils.GetAtomCode(m.GetAtomWithIdx(0))
   >>> c2 = Utils.GetAtomCode(m.GetAtomWithIdx(1))
   >>> c3 = Utils.GetAtomCode(m.GetAtomWithIdx(2))
-  >>> t = 1 | min(c1,c2) << numPathBits | max(c1,c2) << (rdMolDescriptors.AtomPairsParameters.codeSize + numPathBits)
-  >>> pyScorePair(m.GetAtomWithIdx(0), m.GetAtomWithIdx(1), 1)==t
+  >>> t = 1 | min(c1, c2) << numPathBits | max(c1, c2) << (rdMolDescriptors.AtomPairsParameters.codeSize + numPathBits)
+  >>> pyScorePair(m.GetAtomWithIdx(0), m.GetAtomWithIdx(1), 1) == t
   1
-  >>> pyScorePair(m.GetAtomWithIdx(1), m.GetAtomWithIdx(0), 1)==t
+  >>> pyScorePair(m.GetAtomWithIdx(1), m.GetAtomWithIdx(0), 1) == t
   1
-  >>> t = 2 | min(c1,c3) << numPathBits | max(c1,c3) << (rdMolDescriptors.AtomPairsParameters.codeSize + numPathBits)
-  >>> pyScorePair(m.GetAtomWithIdx(0),m.GetAtomWithIdx(2),2)==t
+  >>> t = 2 | min(c1, c3) << numPathBits | max(c1, c3) << (rdMolDescriptors.AtomPairsParameters.codeSize + numPathBits)
+  >>> pyScorePair(m.GetAtomWithIdx(0), m.GetAtomWithIdx(2), 2) == t
   1
-  >>> pyScorePair(m.GetAtomWithIdx(0),m.GetAtomWithIdx(2),2,
+  >>> pyScorePair(m.GetAtomWithIdx(0), m.GetAtomWithIdx(2), 2,
   ...  atomCodes=(Utils.GetAtomCode(m.GetAtomWithIdx(0)), Utils.GetAtomCode(m.GetAtomWithIdx(2)))) == t
   1
 
