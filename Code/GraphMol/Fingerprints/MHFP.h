bd23252540d6a3b53dd6bf8d5515fb3f85282370
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 06:19:27 +0900

    Code/GraphMol/SmilesParse/CXSmilesOps.cpp Code/GraphMol/SmilesParse/SmartsWrite.cpp Code/GraphMol/SmilesParse/catch_tests.cpp Code/GraphMol/SmilesParse/lex.yysmarts.cpp.cmake Code/GraphMol/SmilesParse/smarts.tab.cpp.cmake Code/GraphMol/SmilesParse/smarts.yy Code/GraphMol/SmilesParse/smiles.tab.cpp.cmake

--- Code/GraphMol/Fingerprints/MHFP.h
+++ Code/GraphMol/Fingerprints/MHFP.h
@@ -29,8 +29,8 @@ const uint32_t prime = 0x01000193;
 const uint32_t seed = 0x811C9DC5;
 
 //! A simple implementation of the Fowler–Noll–Vo hash function.
-inline uint32_t hash(const std::string& str, uint32_t hash = seed) {
-  const auto* ptr = (const unsigned char*)str.c_str();
+inline uint32_t hash(const std::string &str, uint32_t hash = seed) {
+  const auto *ptr = (const unsigned char *)str.c_str();
   size_t len = str.length();
 
   while (len--) {
