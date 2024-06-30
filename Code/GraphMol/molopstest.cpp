57c7a9fab7da7fa9daa146bbd11f9ff54454f8dd
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 08:26:26 +0900

    Code/GraphMol/CIPLabeler/catch_tests.cpp Code/GraphMol/ChemReactions/testReaction.cpp Code/GraphMol/DistGeomHelpers/catch_tests.cpp Code/GraphMol/FMCS/testFMCS_Unit.cpp Code/GraphMol/MarvinParse/MarvinDefs.cpp Code/GraphMol/MolDraw2D/catch_tests.cpp Code/GraphMol/MolHash/catch_tests.cpp Code/GraphMol/MolStandardize/testTautomer.cpp Code/GraphMol/RGroupDecomposition/RGroupData.cpp Code/GraphMol/RGroupDecomposition/RGroupUtils.cpp Code/GraphMol/RascalMCES/mces_catch.cpp Code/GraphMol/SmilesParse/catch_tests.cpp Code/GraphMol/SmilesParse/test.cpp Code/GraphMol/Substruct/catch_tests.cpp Code/GraphMol/catch_chirality.cpp Code/GraphMol/molopstest.cpp Code/GraphMol/testChirality.cpp

--- Code/GraphMol/molopstest.cpp
+++ Code/GraphMol/molopstest.cpp
@@ -1635,7 +1635,7 @@ void testIssue183() {
   std::string smi;
   std::string refSmi;
 
-  smi = "Cl\\C(C)=C(\\C(F)=C(/F)C)/C(C)=C(\\F)C";
+  smi = R"(Cl\C(C)=C(\C(F)=C(/F)C)/C(C)=C(\F)C)";
   m2 = SmilesToMol(smi);
   TEST_ASSERT(m2);
   TEST_ASSERT(m2->getBondWithIdx(2)->getStereo() == Bond::STEREOE);
@@ -1780,7 +1780,7 @@ void testIssue190() {
   std::string smi, refSmi;
   int count;
 
-  smi = "O\\N=C\\NC(\\C)=N/OC";
+  smi = R"(O\N=C\NC(\C)=N/OC)";
   m = SmilesToMol(smi);
   TEST_ASSERT(m);
   TEST_ASSERT(m->getBondWithIdx(1)->getBondType() == Bond::DOUBLE);
@@ -1818,7 +1818,7 @@ void testIssue190() {
   TEST_ASSERT(smi == refSmi);
 
   delete m;
-  smi = "O\\N=C\\CC(\\C)=N/OC";
+  smi = R"(O\N=C\CC(\C)=N/OC)";
   m = SmilesToMol(smi);
   TEST_ASSERT(m);
   TEST_ASSERT(m->getBondWithIdx(1)->getBondType() == Bond::DOUBLE);
@@ -1855,7 +1855,7 @@ void testIssue190() {
   TEST_ASSERT(smi == refSmi);
 
   delete m;
-  smi = "O\\N=C\\C(=O)C(\\C)=N/OC";
+  smi = R"(O\N=C\C(=O)C(\C)=N/OC)";
   m = SmilesToMol(smi);
   TEST_ASSERT(m);
   TEST_ASSERT(m->getBondWithIdx(1)->getBondType() == Bond::DOUBLE);
@@ -8042,7 +8042,7 @@ void testRemoveAndTrackIsotopes() {
   };
   // CHEMBL2024142
   auto m =
-      "[2H]C1=C(C(=C2C(=C1[2H])C(=O)C(=C(C2=O)C([2H])([2H])[2H])C/C=C(\\C)/CC([2H])([2H])/C=C(/CC/C=C(\\C)/CCC=C(C)C)\\C([2H])([2H])[2H])[2H])[2H]"_smiles;
+      R"([2H]C1=C(C(=C2C(=C1[2H])C(=O)C(=C(C2=O)C([2H])([2H])[2H])C/C=C(\C)/CC([2H])([2H])/C=C(/CC/C=C(\C)/CCC=C(C)C)\C([2H])([2H])[2H])[2H])[2H])"_smiles;
   TEST_ASSERT(m.get());
   std::unique_ptr<IsotopicHsCount> m_isotopicHsPerHeavy(
       new IsotopicHsCount(*m));
