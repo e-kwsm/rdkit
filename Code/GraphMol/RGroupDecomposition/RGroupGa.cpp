fa53fbe911b9ef73cf709695b61cd2bee61f9c4f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 06:19:27 +0900

    Code/GraphMol/SmilesParse/CXSmilesOps.cpp Code/GraphMol/SmilesParse/SmartsWrite.cpp Code/GraphMol/SmilesParse/catch_tests.cpp Code/GraphMol/SmilesParse/lex.yysmarts.cpp.cmake Code/GraphMol/SmilesParse/smarts.tab.cpp.cmake Code/GraphMol/SmilesParse/smarts.yy Code/GraphMol/SmilesParse/smiles.tab.cpp.cmake

--- Code/GraphMol/RGroupDecomposition/RGroupGa.cpp
+++ Code/GraphMol/RGroupDecomposition/RGroupGa.cpp
@@ -37,7 +37,7 @@ std::string RGroupDecompositionChromosome::info() const {
 }
 
 double RGroupDecompositionChromosome::score() {
-  auto& rGroupData = rGroupGa.getRGroupData();
+  auto &rGroupData = rGroupGa.getRGroupData();
   auto scoreMethod = static_cast<RGroupScore>(rGroupData.params.scoreMethod);
   if (operationName != RgroupMutate) {
     decode();
