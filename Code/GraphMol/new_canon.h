5afa4cced02aa9037cc3f948620051e3b306e747
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 07:48:21 +0900

    feat: modernize-use-nodiscard

--- Code/GraphMol/new_canon.h
+++ Code/GraphMol/new_canon.h
@@ -58,7 +58,7 @@ struct RDKIT_GRAPHMOL_EXPORT bondholder {
         nbrIdx(ni),
         bondIdx(bidx) {}
 
-  int compareStereo(const bondholder &o) const;
+  [[nodiscard]] int compareStereo(const bondholder &o) const;
 
   bool operator<(const bondholder &o) const { return compare(*this, o) < 0; }
   static bool greater(const bondholder &lhs, const bondholder &rhs) {
@@ -281,7 +281,7 @@ unsigned int getChiralRank(const ROMol *dp_mol, canon_atom *dp_atoms,
 }
 }  // namespace
 class RDKIT_GRAPHMOL_EXPORT AtomCompareFunctor {
-  unsigned int getAtomRingNbrCode(unsigned int i) const {
+  [[nodiscard]] unsigned int getAtomRingNbrCode(unsigned int i) const {
     if (!dp_atoms[i].hasRingNbr) {
       return 0;
     }
@@ -296,7 +296,7 @@ class RDKIT_GRAPHMOL_EXPORT AtomCompareFunctor {
     return code;
   }
 
-  int basecomp(int i, int j) const {
+  [[nodiscard]] int basecomp(int i, int j) const {
     unsigned int ivi, ivj;
 
     // always start with the current class:
@@ -574,7 +574,7 @@ class RDKIT_GRAPHMOL_EXPORT ChiralAtomCompareFunctor {
     // FIX: don't want to be doing this long-term
   }
 
-  int basecomp(int i, int j) const {
+  [[nodiscard]] int basecomp(int i, int j) const {
     PRECONDITION(dp_atoms, "no atoms");
     unsigned int ivi, ivj;
 
