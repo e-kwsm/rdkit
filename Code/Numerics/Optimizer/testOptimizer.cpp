f88d2c57345fcb0788dd0acd204ee73e836a8a8e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 09:54:21 +0900

    readability-non-const-parameter

--- Code/Numerics/Optimizer/testOptimizer.cpp
+++ Code/Numerics/Optimizer/testOptimizer.cpp
@@ -14,14 +14,14 @@
 
 #include "BFGSOpt.h"
 
-double circ_0_0(double *v) {
+double circ_0_0(const double *v) {
   double dx = v[0];
   double dy = v[1];
 
   return dx * dx + dy * dy;
 }
 
-double circ_0_0_grad(double *v, double *grad) {
+double circ_0_0_grad(const double *v, double *grad) {
   double dx = v[0];
   double dy = v[1];
   grad[0] = 2 * dx;
@@ -29,14 +29,14 @@ double circ_0_0_grad(double *v, double *grad) {
   return 1.0;
 }
 
-double circ_1_0(double *v) {
+double circ_1_0(const double *v) {
   double dx = v[0] - 1;
   double dy = v[1];
 
   return dx * dx + dy * dy;
 }
 
-double circ_1_0_grad(double *v, double *grad) {
+double circ_1_0_grad(const double *v, double *grad) {
   double dx = v[0] - 1;
   double dy = v[1];
   grad[0] = 2 * dx;
@@ -44,7 +44,7 @@ double circ_1_0_grad(double *v, double *grad) {
   return 1.0;
 }
 
-double func2(double *v) {
+double func2(const double *v) {
   double weight = .5;
   double dx = v[0] - 1;
   double dy = v[1];
@@ -53,7 +53,7 @@ double func2(double *v) {
   return term1 * term1 + weight * term2;
 }
 
-double grad2(double *v, double *grad) {
+double grad2(const double *v, double *grad) {
   double weight = .5;
   double dx = v[0] - 1;
   double dy = v[1];
@@ -67,13 +67,13 @@ double grad2(double *v, double *grad) {
 //   f(x, y) = (x - 3)^2 + (y + 1)^2 - 100
 // The minimum is f = -100 at (3, -1). Useful for exercising the
 // gradient-convergence denominator when the energy is negative.
-double circ_neg(double *v) {
+double circ_neg(const double *v) {
   double dx = v[0] - 3.0;
   double dy = v[1] + 1.0;
   return dx * dx + dy * dy - 100.0;
 }
 
-double circ_neg_grad(double *v, double *grad) {
+double circ_neg_grad(const double *v, double *grad) {
   double dx = v[0] - 3.0;
   double dy = v[1] + 1.0;
   grad[0] = 2 * dx;
@@ -87,8 +87,8 @@ TEST_CASE("testLinearSearch") {
   double grad[2], dir[2];
   double nLoc[2], nVal;
   int resCode;
-  double (*func)(double *);
-  double (*gradFunc)(double *, double *);
+  double (*func)(const double *);
+  double (*gradFunc)(const double *, double *);
 
   func = circ_0_0;
   gradFunc = circ_0_0_grad;
@@ -144,8 +144,8 @@ TEST_CASE("testBFGSOptimization") {
   double oLoc[2], oVal;
   double nVal;
   unsigned int nIters;
-  double (*func)(double *);
-  double (*gradFunc)(double *, double *);
+  double (*func)(const double *);
+  double (*gradFunc)(const double *, double *);
 
   func = circ_0_0;
   gradFunc = circ_0_0_grad;
