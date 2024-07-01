9c5b7daac82bd557bca1d9d21d7e0b540d0d9f2b
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 10:04:43 +0900

    Code/GraphMol/Atom.cpp Code/GraphMol/QueryAtom.h Code/GraphMol/QueryBond.h Code/GraphMol/QueryOps.h Code/GraphMol/ROMol.h Code/GraphMol/RWMol.h

--- Code/GraphMol/QueryOps.h
+++ Code/GraphMol/QueryOps.h
@@ -749,7 +749,7 @@ class RDKIT_GRAPHMOL_EXPORT AtomRingQuery
 class RDKIT_GRAPHMOL_EXPORT RecursiveStructureQuery
     : public Queries::SetQuery<int, Atom const *, true> {
  public:
-  RecursiveStructureQuery() : Queries::SetQuery<int, Atom const *, true>() {
+  RecursiveStructureQuery() {
     setDataFunc(getAtIdx);
     setDescription("RecursiveStructure");
   }
@@ -759,8 +759,7 @@ class RDKIT_GRAPHMOL_EXPORT RecursiveStructureQuery
       - this takes over ownership of the pointer
   */
   RecursiveStructureQuery(ROMol const *query, unsigned int serialNumber = 0)
-      : Queries::SetQuery<int, Atom const *, true>(),
-        d_serialNumber(serialNumber) {
+      : d_serialNumber(serialNumber) {
     setQueryMol(query);
     setDataFunc(getAtIdx);
     setDescription("RecursiveStructure");
@@ -821,7 +820,7 @@ class HasPropQuery : public Queries::EqualityQuery<int, TargetPtr, true> {
   std::string propname;
 
  public:
-  HasPropQuery() : Queries::EqualityQuery<int, TargetPtr, true>(), propname() {
+  HasPropQuery() : Queries::EqualityQuery<int, TargetPtr, true>() {
     // default is to just do a number of rings query:
     this->setDescription("AtomHasProp");
     this->setDataFunc(0);
@@ -870,7 +869,7 @@ class HasPropWithValueQuery
 
  public:
   HasPropWithValueQuery()
-      : Queries::EqualityQuery<int, TargetPtr, true>(), propname(), val() {
+      : Queries::EqualityQuery<int, TargetPtr, true>(), val() {
     // default is to just do a number of rings query:
     this->setDescription("HasPropWithValue");
     this->setDataFunc(0);
@@ -935,8 +934,7 @@ class HasPropWithValueQuery<TargetPtr, std::string>
   std::string val;
 
  public:
-  HasPropWithValueQuery()
-      : Queries::EqualityQuery<int, TargetPtr, true>(), propname(), val() {
+  HasPropWithValueQuery() : Queries::EqualityQuery<int, TargetPtr, true>() {
     // default is to just do a number of rings query:
     this->setDescription("HasPropWithValue");
     this->setDataFunc(0);
@@ -1003,8 +1001,7 @@ class HasPropWithValueQuery<TargetPtr, ExplicitBitVect>
   float tol{0.0};
 
  public:
-  HasPropWithValueQuery()
-      : Queries::EqualityQuery<int, TargetPtr, true>(), propname(), val() {
+  HasPropWithValueQuery() : Queries::EqualityQuery<int, TargetPtr, true>() {
     this->setDescription("HasPropWithValue");
     this->setDataFunc(0);
   }
