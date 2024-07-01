38ad46e0eee75dd7242ab06cd286cd19960e2074
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 10:07:40 +0900

    Code/GraphMol/ChemReactions/ReactionWriter.cpp Code/GraphMol/ChemTransforms/ChemTransforms.cpp Code/GraphMol/FileParsers/CDXMLParser.cpp Code/GraphMol/FileParsers/ForwardSDMolSupplier.cpp Code/GraphMol/FileParsers/MolFileParser.cpp Code/GraphMol/FileParsers/MolSupplier.h Code/GraphMol/FileParsers/MultithreadedSDMolSupplier.cpp Code/GraphMol/FileParsers/SmilesWriter.cpp Code/GraphMol/FileParsers/TplFileParser.cpp Code/GraphMol/Fingerprints/MorganGenerator.cpp Code/GraphMol/FragCatalog/FragCatalogEntry.cpp Code/GraphMol/FragCatalog/FragCatalogEntry.h Code/GraphMol/GenericGroups/GenericGroups.cpp Code/GraphMol/MarvinParse/MarvinDefs.cpp Code/GraphMol/MarvinParse/MarvinParser.cpp Code/GraphMol/MolCatalog/MolCatalogEntry.h Code/GraphMol/MolChemicalFeatures/FeatureParser.cpp Code/GraphMol/MolChemicalFeatures/MolChemicalFeatureDef.h Code/GraphMol/MolDraw2D/DrawMol.cpp Code/GraphMol/MolDraw2D/MolDraw2DHelpers.h Code/GraphMol/MolEnumerator/LinkNode.cpp Code/GraphMol/MolStandardize/Fragment.cpp Code/GraphMol/MolStandardize/FragmentCatalog/FragmentCatalogEntry.h Code/GraphMol/MolStandardize/Tautomer.cpp Code/GraphMol/MolStandardize/Tautomer.h Code/GraphMol/MolStandardize/TransformCatalog/TransformCatalogEntry.h Code/GraphMol/RGroupDecomposition/RGroupScore.cpp Code/GraphMol/RascalMCES/RascalResult.cpp Code/GraphMol/SmilesParse/CXSmilesOps.cpp Code/GraphMol/SmilesParse/SmartsWrite.cpp Code/GraphMol/SmilesParse/SmilesWrite.cpp External/CoordGen/CoordGen.h

--- Code/GraphMol/MarvinParse/MarvinParser.cpp
+++ Code/GraphMol/MarvinParse/MarvinParser.cpp
@@ -676,7 +676,7 @@ class MarvinCMLReader {
     MarvinMolBase *res = nullptr;
 
     try {
-      std::string role = "";
+      std::string role;
 
       if (parentMol == nullptr) {
         res = new MarvinMol(molTree);
