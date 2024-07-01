5f949262d2edaa0137e07304e940f272385936d3
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 10:03:36 +0900

    Code/RDGeneral/Dict.h Code/RDGeneral/RDProps.h

--- Code/RDStreams/streams.cpp
+++ Code/RDStreams/streams.cpp
@@ -3,8 +3,7 @@
 
 namespace RDKit {
 gzstream::gzstream(const std::string &fname)
-    : boost::iostreams::filtering_istream(),
-      is(fname.c_str(), std::ios_base::binary) {
+    : is(fname.c_str(), std::ios_base::binary) {
   push(boost::iostreams::gzip_decompressor());
   push(is);
 }
