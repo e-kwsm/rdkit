793fe5cb251ee7f88bf20182c80bfeaef9db2b74
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 10:07:23 +0900

    Code/DataStructs/testFPB.cpp Code/GraphMol/ChemReactions/Enumerate/EnumerateBase.h Code/GraphMol/ChemReactions/MoleculeParser.cpp Code/GraphMol/ChemReactions/Reaction.cpp Code/GraphMol/ChemReactions/ReactionRunner.cpp Code/GraphMol/ChemReactions/ReactionUtils.cpp Code/GraphMol/ChemReactions/ReactionWriter.cpp Code/GraphMol/ChemReactions/testReaction.cpp Code/GraphMol/ChemTransforms/testChemTransforms.cpp Code/GraphMol/Deprotect/Deprotect.h Code/GraphMol/Descriptors/Crippen.cpp Code/GraphMol/FileParsers/cdxml_parser_catch.cpp Code/GraphMol/FilterCatalog/FilterCatalogRunner.cpp Code/GraphMol/FilterCatalog/FilterMatchers.h Code/GraphMol/ForceFieldHelpers/CrystalFF/TorsionPreferences.cpp Code/GraphMol/FragCatalog/FragCatParams.cpp Code/GraphMol/MMPA/MMPA_UnitTest.cpp Code/GraphMol/MarvinParse/MarvinParser.cpp Code/GraphMol/MolAlign/Wrap/rdMolAlign.cpp Code/GraphMol/MolInterchange/Writer.cpp Code/GraphMol/RGroupDecomposition/testRGroupDecomp.cpp Code/GraphMol/Resonance.cpp Code/GraphMol/ScaffoldNetwork/ScaffoldNetwork.cpp Code/GraphMol/SubstructLibrary/PatternFactory.cpp Code/GraphMol/SubstructLibrary/SubstructLibrary.h Code/GraphMol/SubstructLibrary/SubstructLibrarySerialization.h Code/GraphMol/SubstructLibrary/substructLibraryTest.cpp Code/GraphMol/Wrap/MolOps.cpp Code/GraphMol/Wrap/rdmolfiles.cpp

--- Code/GraphMol/ChemReactions/Reaction.cpp
+++ Code/GraphMol/ChemReactions/Reaction.cpp
@@ -674,7 +674,7 @@ void ChemicalReaction::removeUnmappedReactantTemplates(
   MOL_SPTR_VECT res_reactantTemplates;
   for (auto iter = beginReactantTemplates(); iter != endReactantTemplates();
        ++iter) {
-    if (isReactionTemplateMoleculeAgent(*iter->get(), thresholdUnmappedAtoms)) {
+    if (isReactionTemplateMoleculeAgent(**iter, thresholdUnmappedAtoms)) {
       if (moveToAgentTemplates) {
         m_agentTemplates.push_back(*iter);
       }
@@ -698,7 +698,7 @@ void ChemicalReaction::removeUnmappedProductTemplates(
   MOL_SPTR_VECT res_productTemplates;
   for (auto iter = beginProductTemplates(); iter != endProductTemplates();
        ++iter) {
-    if (isReactionTemplateMoleculeAgent(*iter->get(), thresholdUnmappedAtoms)) {
+    if (isReactionTemplateMoleculeAgent(**iter, thresholdUnmappedAtoms)) {
       if (moveToAgentTemplates) {
         m_agentTemplates.push_back(*iter);
       }
