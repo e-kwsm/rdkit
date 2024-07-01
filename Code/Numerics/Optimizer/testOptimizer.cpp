de57e933ece47fefd8fd761c2da1ed1365a9a980
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 09:55:32 +0900

    Code/ForceField/ForceField.cpp Code/ForceField/ForceField.h Code/ForceField/MMFF/AngleBend.cpp Code/ForceField/MMFF/AngleBend.h Code/ForceField/MMFF/TorsionAngle.cpp Code/ForceField/MMFF/TorsionAngle.h Code/ForceField/UFF/AngleBend.cpp Code/ForceField/UFF/AngleBend.h Code/ForceField/UFF/TorsionAngle.cpp Code/ForceField/UFF/TorsionAngle.h Code/Geometry/Transform3D.cpp Code/Geometry/Transform3D.h Code/GraphMol/Chirality.cpp Code/GraphMol/Descriptors/AUTOCORR3D.cpp Code/GraphMol/Descriptors/DCLV.cpp Code/GraphMol/Descriptors/EEM.cpp Code/GraphMol/DistGeomHelpers/BoundsMatrixBuilder.cpp Code/GraphMol/MolHash/hashfunctions.cpp Code/GraphMol/SLNParse/sln.tab.cpp.cmake Code/GraphMol/SmilesParse/smarts.tab.cpp.cmake Code/GraphMol/SmilesParse/smiles.tab.cpp.cmake Code/GraphMol/Subgraphs/Subgraphs.cpp Code/GraphMol/new_canon.cpp Code/GraphMol/new_canon.h Code/ML/Cluster/Murtagh/Clustering.cpp Code/ML/Data/cQuantize.cpp Code/ML/InfoTheory/InfoBitRanker.cpp Code/ML/InfoTheory/InfoBitRanker.h Code/Numerics/Optimizer/BFGSOpt.h Code/Numerics/Optimizer/testOptimizer.cpp

--- Code/Numerics/Optimizer/testOptimizer.cpp
+++ Code/Numerics/Optimizer/testOptimizer.cpp
@@ -15,14 +15,14 @@
 
 #include "BFGSOpt.h"
 
-double circ_0_0(double *v) {
+double circ_0_0(const double *v) {
   double dx = v[0];
   double dy = v[1];
 
   return dx * dx + dy * dy;
 }
 
-double circ_0_0_grad(double *v, double *grad) {
+double circ_0_0_grad(const double *v, double *grad) {
   double dx = v[0];
   double dy = v[1];
   grad[0] = 2 * dx;
@@ -30,14 +30,14 @@ double circ_0_0_grad(double *v, double *grad) {
   return 1.0;
 }
 
-double circ_1_0(double *v) {
+double circ_1_0(const double *v) {
   double dx = v[0] - 1;
   double dy = v[1];
 
   return dx * dx + dy * dy;
 }
 
-double circ_1_0_grad(double *v, double *grad) {
+double circ_1_0_grad(const double *v, double *grad) {
   double dx = v[0] - 1;
   double dy = v[1];
   grad[0] = 2 * dx;
@@ -45,7 +45,7 @@ double circ_1_0_grad(double *v, double *grad) {
   return 1.0;
 }
 
-double func2(double *v) {
+double func2(const double *v) {
   double weight = .5;
   double dx = v[0] - 1;
   double dy = v[1];
@@ -54,7 +54,7 @@ double func2(double *v) {
   return term1 * term1 + weight * term2;
 }
 
-double grad2(double *v, double *grad) {
+double grad2(const double *v, double *grad) {
   double weight = .5;
   double dx = v[0] - 1;
   double dy = v[1];
@@ -73,8 +73,8 @@ void test1() {
   double grad[2], dir[2];
   double nLoc[2], nVal;
   int resCode;
-  double (*func)(double *);
-  double (*gradFunc)(double *, double *);
+  double (*func)(const double *);
+  double (*gradFunc)(const double *, double *);
 
   func = circ_0_0;
   gradFunc = circ_0_0_grad;
@@ -136,8 +136,8 @@ void test2() {
   double oLoc[2], oVal;
   double nVal;
   unsigned int nIters;
-  double (*func)(double *);
-  double (*gradFunc)(double *, double *);
+  double (*func)(const double *);
+  double (*gradFunc)(const double *, double *);
 
   func = circ_0_0;
   gradFunc = circ_0_0_grad;
