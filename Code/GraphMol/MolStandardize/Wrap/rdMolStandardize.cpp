4aa6cc98fbd8e57e4a4168476436a040157e4afa
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-01 17:40:37 +0900

    Code/GraphMol/MolStandardize/Wrap/rdMolStandardize.cpp

--- Code/GraphMol/MolStandardize/Wrap/rdMolStandardize.cpp
+++ Code/GraphMol/MolStandardize/Wrap/rdMolStandardize.cpp
@@ -361,9 +361,8 @@ RDKit::ROMol *disconnectOrganometallicsHelper(RDKit::ROMol &mol,
         python::extract<RDKit::MolStandardize::MetalDisconnectorOptions *>(
             params);
     return RDKit::MolStandardize::disconnectOrganometallics(mol, *mdo);
-  } else {
-    return RDKit::MolStandardize::disconnectOrganometallics(mol);
   }
+  return RDKit::MolStandardize::disconnectOrganometallics(mol);
 }
 void disconnectOrganometallicsInPlaceHelper(RDKit::ROMol *mol,
                                             python::object params) {
@@ -373,10 +372,9 @@ void disconnectOrganometallicsInPlaceHelper(RDKit::ROMol *mol,
             params);
     return RDKit::MolStandardize::disconnectOrganometallicsInPlace(
         *static_cast<RDKit::RWMol *>(mol), *mdo);
-  } else {
-    return RDKit::MolStandardize::disconnectOrganometallicsInPlace(
-        *static_cast<RDKit::RWMol *>(mol));
   }
+  return RDKit::MolStandardize::disconnectOrganometallicsInPlace(
+      *static_cast<RDKit::RWMol *>(mol));
 }
 
 }  // namespace
