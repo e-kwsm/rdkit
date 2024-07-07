6224fb4c3ed1eb170a82556c3cad2a6abe338b81
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-08 05:24:23 +0900

    Code/GraphMol/FileParsers/XYZFileParser.cpp

--- Code/GraphMol/FileParsers/XYZFileParser.cpp
+++ Code/GraphMol/FileParsers/XYZFileParser.cpp
@@ -158,9 +158,8 @@ std::unique_ptr<RWMol> MolFromXYZBlock(const std::string &xyzBlock) {
   xyz.peek();
   if (!xyz.eof()) {
     return MolFromXYZDataStream(xyz);
-  } else {
-    return nullptr;
   }
+  return nullptr;
 }
 
 std::unique_ptr<RWMol> MolFromXYZFile(const std::string &fName) {
@@ -174,9 +173,8 @@ std::unique_ptr<RWMol> MolFromXYZFile(const std::string &fName) {
   xyzFile.peek();
   if (!xyzFile.eof()) {
     return MolFromXYZDataStream(xyzFile);
-  } else {
-    return nullptr;
   }
+  return nullptr;
 }
 }  // namespace FileParsers
 }  // namespace v2
