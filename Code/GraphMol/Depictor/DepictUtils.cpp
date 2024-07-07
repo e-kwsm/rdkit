781c9cef501157ac65c22c839406557aa182293f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-08 05:01:40 +0900

    Code/GraphMol/Depictor/DepictUtils.cpp

--- Code/GraphMol/Depictor/DepictUtils.cpp
+++ Code/GraphMol/Depictor/DepictUtils.cpp
@@ -471,7 +471,7 @@ INT_PAIR_VECT findBondsPairsToPermuteDeg4(const RDGeom::Point2D &center,
       res.push_back(p2);
     }
     return res;
-  } else {
+  }
     // bids[0] and bids[1] are opposite to each other, so bids[2] and bids[3]
     // must
     // be perpendicular to bids[0]
@@ -480,7 +480,6 @@ INT_PAIR_VECT findBondsPairsToPermuteDeg4(const RDGeom::Point2D &center,
     INT_PAIR p2(nbrBids[0], nbrBids[3]);
     res.push_back(p2);
     return res;
-  }
 }
 
 template <class T>
