c67eb6a22d2c0180315c2456c65c1614d1d13319
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-08 04:19:38 +0900

    Code/GraphMol/PeriodicTable.h

--- Code/GraphMol/PeriodicTable.h
+++ Code/GraphMol/PeriodicTable.h
@@ -248,9 +248,8 @@ class RDKIT_GRAPHMOL_EXPORT PeriodicTable {
         m.find(isotope);
     if (item == m.end()) {
       return 0.0;
-    } else {
-      return item->second.first;
     }
+    return item->second.first;
   }
   //! \overload
   double getMassForIsotope(const std::string &elementSymbol,
@@ -273,9 +272,8 @@ class RDKIT_GRAPHMOL_EXPORT PeriodicTable {
         m.find(isotope);
     if (item == m.end()) {
       return 0.0;
-    } else {
-      return item->second.second;
     }
+    return item->second.second;
   }
   //! \overload
   double getAbundanceForIsotope(const std::string &elementSymbol,
