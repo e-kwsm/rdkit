1a8cd13d36dc19c8f2467c421c4196511a7f1ef2
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-08 05:24:15 +0900

    Code/GraphMol/FileParsers/MultithreadedSDMolSupplier.cpp

--- Code/GraphMol/FileParsers/MultithreadedSDMolSupplier.cpp
+++ Code/GraphMol/FileParsers/MultithreadedSDMolSupplier.cpp
@@ -218,7 +218,7 @@ void MultithreadedSDMolSupplier::readMolProps(RWMol &mol,
           // FIX: should we be deleting the molecule (which is probably fine)
           // because we couldn't read the data ???
           throw FileParseException("Problems encountered parsing data fields");
-        } else {
+        }
           if (!warningIssued) {
             if (hasProp) {
               BOOST_LOG(rdWarningLog)
@@ -230,7 +230,6 @@ void MultithreadedSDMolSupplier::readMolProps(RWMol &mol,
             }
             warningIssued = true;
           }
-        }
       }
     }
     std::getline(inStream, tempStr);
