58e001b46e37314c5f59ac3000018ed8b0f7dfc0
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-08 04:57:58 +0900

    Code/GraphMol/SubstanceGroup.cpp

--- Code/GraphMol/SubstanceGroup.cpp
+++ Code/GraphMol/SubstanceGroup.cpp
@@ -213,12 +213,11 @@ SubstanceGroup::BondType SubstanceGroup::getBondType(
   }
   if (begin_atom_in_sgroup || end_atom_in_sgroup) {
     return SubstanceGroup::BondType::XBOND;
-  } else {
+  }
     std::ostringstream errout;
     errout << "Neither beginning nor ending atoms of bond " << bond->getIdx()
            << " is in this SubstanceGroup.";
     throw SubstanceGroupException(errout.str());
-  }
 }
 
 bool SubstanceGroup::adjustToRemovedAtom(unsigned int atomIdx) {
