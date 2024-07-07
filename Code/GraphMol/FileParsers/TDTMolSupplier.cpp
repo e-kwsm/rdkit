3abc0f869257e272e1fbee77a5d1695a29961b68
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-08 05:16:13 +0900

    Code/GraphMol/FileParsers/TDTMolSupplier.cpp

--- Code/GraphMol/FileParsers/TDTMolSupplier.cpp
+++ Code/GraphMol/FileParsers/TDTMolSupplier.cpp
@@ -57,9 +57,8 @@ void ParseNumberList(std::string inLine, std::vector<T> &res,
     }
     if (foundEnd || !inStream || inStream->eof()) {
       break;
-    } else {
-      std::getline(*inStream, inLine);
     }
+    std::getline(*inStream, inLine);
   }
   if (!foundEnd) {
     throw FileParseException("no end tag found for numeric list");
@@ -244,13 +243,12 @@ std::unique_ptr<RWMol> TDTMolSupplier::parseMol(std::string inLine) {
           std::ostringstream errout;
           errout << "no end tag found for property" << propName;
           throw FileParseException(errout.str());
-        } else {
+        }
           std::string propVal = inLine.substr(startP, endP - startP);
           res->setProp(propName, propVal);
           if (propName == d_params.nameRecord) {
             res->setProp(common_properties::_Name, propVal);
           }
-        }
       }
       std::getline(*dp_inStream, inLine);
     }
@@ -382,7 +380,7 @@ unsigned int TDTMolSupplier::length() {
   // return the number of mol blocks in the sdfile
   if (d_len > 0) {
     return d_len;
-  } else {
+  }
     std::string tempStr;
     d_len = d_molpos.size();
     dp_inStream->seekg(d_molpos.back());
@@ -397,7 +395,6 @@ unsigned int TDTMolSupplier::length() {
     dp_inStream->clear();
     dp_inStream->seekg(d_molpos[d_last]);
     return d_len;
-  }
 }
 
 bool TDTMolSupplier::atEnd() {
