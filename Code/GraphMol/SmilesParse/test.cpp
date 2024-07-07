be30f19fdcc1eb9510e7e8bdb8bb04fdf4700ed3
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-08 07:20:42 +0900

    Code/GraphMol/SmilesParse/cxsmiles_test.cpp Code/GraphMol/SmilesParse/test.cpp

--- Code/GraphMol/SmilesParse/test.cpp
+++ Code/GraphMol/SmilesParse/test.cpp
@@ -3661,7 +3661,7 @@ void testEmptyStrings() {
                        << std::endl;
   {
     RWMol *m;
-    std::string smiles = "";
+    std::string smiles;
     m = SmilesToMol(smiles);
     TEST_ASSERT(m);
     TEST_ASSERT(m->getNumAtoms() == 0);
@@ -3672,7 +3672,7 @@ void testEmptyStrings() {
   }
   {
     RWMol *m;
-    std::string smiles = "";
+    std::string smiles;
     m = SmartsToMol(smiles);
     TEST_ASSERT(m);
     TEST_ASSERT(m->getNumAtoms() == 0);
