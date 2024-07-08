0b4887eca452be6398f4a36c53cdd06d2bf2ef9d
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-09 06:50:11 +0900

    Code/GraphMol/ChemReactions/PreprocessRxn.cpp

--- Code/GraphMol/ChemReactions/PreprocessRxn.cpp
+++ Code/GraphMol/ChemReactions/PreprocessRxn.cpp
@@ -60,7 +60,8 @@ bool preprocessReaction(
 bool preprocessReaction(ChemicalReaction &rxn,
                         const std::map<std::string, ROMOL_SPTR> &queries,
                         const std::string &propName) {
-  unsigned int numWarnings, numErrors;
+  unsigned int numWarnings;
+  unsigned int numErrors;
   std::vector<std::vector<std::pair<unsigned int, std::string>>> reactantLabels;
 
   return preprocessReaction(rxn, numWarnings, numErrors, reactantLabels,
