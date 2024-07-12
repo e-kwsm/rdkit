fa62999e75c9ebbfd82b7bf54d2eb806ae2b23b2
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-12 14:20:34 +0900

    Code/

--- Code/GraphMol/MolDraw2D/catch_tests.cpp
+++ Code/GraphMol/MolDraw2D/catch_tests.cpp
@@ -2749,7 +2749,7 @@ M  END)CTAB"_ctab;
     std::vector<int> rotns = {0, 30, 60, 90, 120, 150, 180};
     for (auto rotn : rotns) {
       MolDraw2DSVG drawer(350, 300);
-      drawer.drawOptions().rotate = (double)rotn;
+      drawer.drawOptions().rotate = static_cast<double>(rotn);
       drawer.drawMolecule(*m);
       drawer.finishDrawing();
       auto text = drawer.getDrawingText();
@@ -2996,7 +2996,7 @@ M  END)CTAB"_ctab;
     std::vector<int> rotns = {0, 30, 60, 90, 120, 150, 180};
     for (auto rotn : rotns) {
       MolDraw2DSVG drawer(350, 300);
-      drawer.drawOptions().rotate = (double)rotn;
+      drawer.drawOptions().rotate = static_cast<double>(rotn);
       drawer.drawMolecule(*m);
       drawer.finishDrawing();
       auto text = drawer.getDrawingText();
