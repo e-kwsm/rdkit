70d604bbe45f8e1d5170b1772ab3693136138aeb
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-07-12 13:46:39 +0900

    Code/GraphMol/SmilesParse/lex.yysmarts.cpp.cmake Code/GraphMol/SmilesParse/lex.yysmiles.cpp.cmake Code/GraphMol/SmilesParse/smarts.tab.cpp.cmake Code/GraphMol/SmilesParse/smarts.yy Code/GraphMol/SmilesParse/smiles.tab.cpp.cmake

--- Code/GraphMol/SmilesParse/lex.yysmiles.cpp.cmake
+++ Code/GraphMol/SmilesParse/lex.yysmiles.cpp.cmake
@@ -539,7 +539,7 @@ void yyfree ( void * , yyscan_t yyscanner );
 
 /* Begin user sect3 */
 
-#define yysmiles_wrap(yyscanner) (/*CONSTCOND*/1)
+#define yysmiles_wrap(yyscanner) (/*CONSTCOND*/ true)
 #define YY_SKIP_YYWRAP
 typedef flex_uint8_t YY_CHAR;
 
@@ -1168,7 +1168,7 @@ YY_DECL
 
 #line 1169 "/localhome/glandrum/RDKit_git/Code/GraphMol/SmilesParse/lex.yysmiles.cpp"
 
-	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
+	while ( /*CONSTCOND*/ true )		/* loops until end-of-file is reached */
 		{
 		yy_cp = yyg->yy_c_buf_p;
 
