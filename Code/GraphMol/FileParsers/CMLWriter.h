fba4c8150a49055ac437d4b91975dfb4b138b1c9
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-01 05:19:13 +0900

    Code/GraphMol/FileParsers/CMLWriter.h

---
+++ Code/GraphMol/FileParsers/CMLWriter.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iosfwd>
+#include <memory>
+#include <string>
+
+#include <boost/property_tree/ptree.hpp>
+
+#include <RDGeneral/export.h>
+#include <GraphMol/ROMol.h>
+#include <GraphMol/RWMol.h>
+
+namespace RDKit {
+class RDKIT_FILEPARSERS_EXPORT CMLWriter {
+ public:
+  CMLWriter() = delete;
+  CMLWriter(const std::string &fileName);
+  CMLWriter(std::unique_ptr<std::ostream> &&p);
+  CMLWriter(const CMLWriter &) = delete;
+  ~CMLWriter();
+
+  CMLWriter &operator=(const CMLWriter &) = delete;
+
+  /// add the molecule to the CML document
+  void add_molecule(const ROMol &mol, int confId = -1);
+  /// write CML
+  void write() const;
+
+ private:
+  std::unique_ptr<std::ostream> p_ostream;
+  boost::property_tree::ptree tree;
+  unsigned int num_written_mols = 0u;
+  static constexpr auto molecule_id_prefix = "m";
+  static constexpr auto atom_id_prefix = "a";
+  static constexpr auto bond_id_prefix = "b";
+};
+}  // namespace RDKit
