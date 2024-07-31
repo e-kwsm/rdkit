7eaa8a2bf3f0c96b9a592eeaf573e4b4af152a72
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-01 07:14:52 +0900

    CMLW/Makefile CMLW/tmp.eRJHXVzYXD.cpp Code/GraphMol/FileParsers/CMLWriter.h

--- CMLW/tmp.eRJHXVzYXD.cpp
+++ CMLW/tmp.eRJHXVzYXD.cpp
@@ -1,35 +1,80 @@
 #include "CMLWriter.h"
 #include <fstream>
 
+#include <iostream>
+
+#include <boost/format.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
 #include <RDGeneral/BadFileException.h>
 #include <RDGeneral/FileParseException.h>
+#include <GraphMol/RWMol.h>
 
 namespace RDKit {
 CMLWriter::CMLWriter(const std::string &fileName)
     : CMLWriter{std::make_unique<std::ofstream>(fileName)} {}
 
-CMLWriter::CMLWriter(std::unique_ptr<std::ostream> &&p_os)
-    : p_ostream{std::move(p_os)} {
-  PRECONDITION(p_os, "null stream");
-  if (p_os->bad()) {
+CMLWriter::CMLWriter(std::unique_ptr<std::ostream> &&p)
+    : p_ostream{std::move(p)} {
+  PRECONDITION(p_ostream, "null stream");
+  if (p_ostream->bad()) {
     throw FileParseException("Bad output stream");
   }
+  auto &cml = pt.add("cml", "");
+  cml.put("<xmlattr>.xmlns", "http://www.xml-cml.org/schema");
+  cml.put("<xmlattr>.xmlns:convention", "http://www.xml-cml.org/convention/");
+  cml.put("<xmlattr>.convention", "convention:molecular");
 }
 
-CMLWriter::~CMLWriter() { close(); }
+CMLWriter::~CMLWriter() { write(); }
 
-void CMLWriter::write(const ROMol &mol, int confId) {
+void CMLWriter::write() const {
   PRECONDITION(p_ostream, "no output stream");
+  boost::property_tree::write_xml(
+      *p_ostream, pt,
+      boost::property_tree::xml_writer_make_settings<std::string>(' ', 2));
 }
 
-void CMLWriter::flush() const {
-  if (p_ostream) {
-    p_ostream->flush();
+void CMLWriter::add(const ROMol &mol, int confId) {
+  PRECONDITION(p_ostream, "no output stream");
+  RWMol rwmol{mol};
+  auto &molecule_node = pt.add("cml.molecule", "");
+  molecule_node.put("<xmlattr>.id", boost::format{"%1%.%2%"} % "m" % confId);
+
+  constexpr auto atom_id_prefix = "a";
+
+  const Conformer *conf = nullptr;
+  if (rwmol.getNumConformers()) {
+    conf = &rwmol.getConformer(confId);
   }
-}
+  auto &atomArray = molecule_node.put("atomArray", "");
+  for (unsigned i = 0u, nAtoms = rwmol.getNumAtoms(); i < nAtoms; i++) {
+    auto &atom = atomArray.add("atom", "");
+    const auto &a = rwmol.getAtomWithIdx(i);
 
-void CMLWriter::close() {
-  flush();
-  p_ostream.reset();
+    atom.put("<xmlattr>.id", boost::format{"%1%%2%"} % atom_id_prefix % i);
+    if (a->getAtomicNum()) {
+      atom.put("<xmlattr>.elementType", a->getSymbol());
+    } else {
+      atom.put("<xmlattr>.elementType", "Du");  // dummy
+    }
+
+    if (conf != nullptr) {
+      const auto &pos = conf->getAtomPos(i);
+      boost::format xyz_fmt{"%.6f"};
+
+      if (!conf->is3D()) {
+        atom.put("<xmlattr>.x2", xyz_fmt % pos.x);
+        atom.put("<xmlattr>.y2", xyz_fmt % pos.y);
+      } else {
+        atom.put("<xmlattr>.x3", xyz_fmt % pos.x);
+        atom.put("<xmlattr>.y3", xyz_fmt % pos.y);
+        atom.put("<xmlattr>.z3", xyz_fmt % pos.z);
+      }
+    }
+  }
+
+  auto &bondArray = molecule_node.put("bondArray", "");
+  bondArray.add("atom", "");
 }
 }  // namespace RDKit
