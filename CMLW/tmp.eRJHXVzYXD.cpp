a0e7ab297cee4f46d8bb310a34cb52308562fb24
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-01 05:38:25 +0900

    CMLW/tmp.eRJHXVzYXD.cpp Code/GraphMol/FileParsers/CMLWriter.h

--- CMLW/tmp.eRJHXVzYXD.cpp
+++ CMLW/tmp.eRJHXVzYXD.cpp
@@ -9,7 +9,7 @@ CMLWriter::CMLWriter(const std::string &fileName)
     : CMLWriter{std::make_unique<std::ofstream>(fileName)} {}
 
 CMLWriter::CMLWriter(std::unique_ptr<std::ostream> &&p_os)
-    : p_os{std::move(p_os)} {
+    : p_ostream{std::move(p_os)} {
   PRECONDITION(p_os, "null stream");
   if (p_os->bad()) {
     throw FileParseException("Bad output stream");
@@ -18,14 +18,18 @@ CMLWriter::CMLWriter(std::unique_ptr<std::ostream> &&p_os)
 
 CMLWriter::~CMLWriter() { close(); }
 
+void CMLWriter::write(const ROMol &mol, int confId) {
+  PRECONDITION(p_ostream, "no output stream");
+}
+
 void CMLWriter::flush() const {
-  if (p_os) {
-    p_os->flush();
+  if (p_ostream) {
+    p_ostream->flush();
   }
 }
 
 void CMLWriter::close() {
   flush();
-  p_os.reset();
+  p_ostream.reset();
 }
 }  // namespace RDKit
