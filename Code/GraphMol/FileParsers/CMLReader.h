2f193fdb058db43cda61c163478bb66635a6c244
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-05 10:40:35 +0900

    Code/GraphMol/FileParsers/CMLReader.cpp Code/GraphMol/FileParsers/CMLReader.h

---
+++ Code/GraphMol/FileParsers/CMLReader.h
@@ -0,0 +1,32 @@
+//  Copyright (C) 2020 Eisuke Kawashima
+//
+//  Chemical Markup Language, CML, schema 3
+//  http://www.xml-cml.org/
+//  See
+//  http://www.xml-cml.org/convention/molecular
+//  http://www.xml-cml.org/schema/schema3/schema.xsd
+//
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+
+#pragma once
+
+#include <RDGeneral/export.h>
+
+namespace RDKit {
+namespace v2 {
+namespace FileParsers {
+class RDKIT_FILEPARSERS_EXPORT CMLSupplier {
+ public:
+  CMLSupplier() = delete;
+  ~CMLSupplier();
+  CMLSupplier(const CMLSupplier &) = delete;
+  CMLSupplier &operator=(const CMLSupplier &) = delete;
+};
+}  // namespace FileParsers
+}  // namespace v2
+}  // namespace RDKit
