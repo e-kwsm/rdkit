5aabac09a00a06f150b452fca71644f99e68e8b5
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-05 14:56:22 +0900

    CMLR/CMLReader.cpp CMLR/CMLReader.h

---
+++ CMLR/CMLReader.h
@@ -0,0 +1,58 @@
+//  Copyright (C) 2020 Eisuke Kawashima
+//
+//  Chemical Markup Language, CML, schema 3
+//  http://www.xml-cml.org/
+//  See
+//  http://www.xml-cml.org/convention/molecular
+//  http://www.xml-cml.org/schema/schema3/schema.xsd
+//
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <boost/property_tree/ptree.hpp>
+
+namespace RDKit {
+class Atom;
+class Conformer;
+class RWMol;
+
+namespace v2 {
+namespace FileParsers {
+namespace cml {
+class CMLMolecule {
+ public:
+  CMLMolecule(const boost::property_tree::ptree &molecule_node);
+  ~CMLMolecule();
+
+  std::unique_ptr<RWMol> parse();
+
+ private:
+  void parse_atomArray(const boost::property_tree::ptree &node);
+  void parse_atom(const boost::property_tree::ptree &node);
+  void parse_bondArray(const boost::property_tree::ptree &node);
+  void parse_bond(const boost::property_tree::ptree &node);
+
+  boost::property_tree::ptree molecule_node;
+
+  std::unique_ptr<RDKit::RWMol> molecule;
+  std::unique_ptr<RDKit::Conformer> conformer;
+  std::unordered_map<std::string, std::unique_ptr<RDKit::Atom>> id_atom;
+  std::unordered_map<std::string, boost::optional<unsigned>> id_hydrogenCount;
+  std::unordered_set<std::string> bond_ids;
+  int sum_formalCharge = 0;
+};
+}  // namespace cml
+}  // namespace FileParsers
+}  // namespace v2
+}  // namespace RDKit
