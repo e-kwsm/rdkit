a760575e427be12cb2aee43e5165cb857b46c1ae
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-09 03:08:36 +0900

    Code/ForceField/MMFF/AngleConstraint.h Code/ForceField/MMFF/DistanceConstraint.h Code/ForceField/MMFF/PositionConstraint.h Code/ForceField/UFF/AngleConstraint.h Code/ForceField/UFF/Inversions.cpp Code/ForceField/UFF/Inversions.h Code/ForceField/UFF/Utils.cpp Code/ForceField/UFF/Utils.h Code/GraphMol/ForceFieldHelpers/CrystalFF/TorsionAngleContribs.cpp Code/GraphMol/ForceFieldHelpers/CrystalFF/TorsionAngleContribs.h Code/GraphMol/MolProps.cpp Code/GraphMol/MolStandardize/Pipeline.cpp Code/GraphMol/MolStandardize/Pipeline.h Code/GraphMol/MolStandardize/Wrap/Pipeline.cpp Code/GraphMol/catch_graphmol.cpp Code/MinimalLib/common.h Code/MinimalLib/common_defs.h

--- Code/MinimalLib/common.h
+++ Code/MinimalLib/common.h
@@ -84,8 +84,7 @@
 
 namespace rj = rapidjson;
 
-namespace RDKit {
-namespace MinimalLib {
+namespace RDKit::MinimalLib {
 
 static constexpr int d_defaultWidth = 250;
 static constexpr int d_defaultHeight = 200;
@@ -1234,7 +1233,7 @@ struct LogHandle {
   static LoggingFlag d_loggingNeedsInit;
 };
 
-}  // namespace MinimalLib
-}  // namespace RDKit
+}  // namespace RDKit::MinimalLib
+
 #undef LPT_OPT_GET
 #undef LPT_OPT_GET2
