8d0e13691c1154c044fcf6b17531f7c7e1cb469d
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-09 16:20:08 +0900

    master

--- Code/DataStructs/FPBReader.cpp
+++ Code/DataStructs/FPBReader.cpp
@@ -14,18 +14,13 @@
 
 #include <DataStructs/ExplicitBitVect.h>
 #include <DataStructs/BitOps.h>
+
 #include <RDGeneral/Invariant.h>
 #include <RDGeneral/StreamOps.h>
 #include <RDGeneral/Ranking.h>
-#include <cmath>
-
 #include "FPBReader.h"
 #include <boost/scoped_ptr.hpp>
 #include <boost/scoped_array.hpp>
-#include "RDGeneral/BadFileException.h"
-#include "RDGeneral/Exceptions.h"
-#include "RDGeneral/RDLog.h"
-#include "RDGeneral/export.h"
 
 namespace RDKit {
 
