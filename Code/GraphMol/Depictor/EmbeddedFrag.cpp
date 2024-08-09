252a9119b26715cf012edc754924911d7ea8a53e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-09 15:43:52 +0900

    Code/GraphMol/Depictor/EmbeddedFrag.cpp

--- Code/GraphMol/Depictor/EmbeddedFrag.cpp
+++ Code/GraphMol/Depictor/EmbeddedFrag.cpp
@@ -2028,10 +2028,9 @@ void EmbeddedFrag::removeCollisionsBondFlip() {
             }
             if (colls.size() == ncols && newDensity < prevDensity) {
               break;
-            } else {
-              flipAboutBond(ri, false);
-              colls = this->findCollisions(dmat);
             }
+            flipAboutBond(ri, false);
+            colls = this->findCollisions(dmat);
         }
       }
     }
