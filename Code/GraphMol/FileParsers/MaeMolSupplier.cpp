4adc3191a3a91ce93e8445a433a0f4b951d63942
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-09 15:49:11 +0900

    Code/GraphMol/FileParsers/MaeMolSupplier.cpp Code/GraphMol/FileParsers/MaeWriter.cpp Code/GraphMol/FileParsers/MaestroProperties.h Code/GraphMol/FileParsers/ProximityBonds.cpp Code/GraphMol/FileParsers/XYZFileParser.cpp Code/GraphMol/MolInterchange/details.h

--- Code/GraphMol/FileParsers/MaeMolSupplier.cpp
+++ Code/GraphMol/FileParsers/MaeMolSupplier.cpp
@@ -8,20 +8,20 @@
 //  of the RDKit source tree.
 //
 #include <cstring>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 #include <boost/unordered_set.hpp>
 
-#include <RDGeneral/BadFileException.h>
-#include <RDGeneral/FileParseException.h>
+#include <GraphMol/FileParsers/FileParserUtils.h>
+#include <GraphMol/FileParsers/MaestroProperties.h>
+#include <GraphMol/FileParsers/MolSupplier.h>
 #include <GraphMol/MolInterchange/details.h>
 #include <GraphMol/MolOps.h>
 #include <GraphMol/MonomerInfo.h>
 #include <GraphMol/RWMol.h>
-#include <GraphMol/FileParsers/MaestroProperties.h>
-#include <GraphMol/FileParsers/MolSupplier.h>
-#include <GraphMol/FileParsers/FileParserUtils.h>
+#include <RDGeneral/BadFileException.h>
+#include <RDGeneral/FileParseException.h>
 
 #include <boost/tokenizer.hpp>
 
@@ -213,7 +213,8 @@ std::string strip_prefix_from_mae_property(const std::string &propName) {
     ++propNamePtr;
     if (strncmp(propNamePtr, "_rdk_", 5) == 0) {
       return propName.substr(6);
-    } else if (strncmp(propNamePtr, "_rdkit_", 7) == 0) {
+    }
+    if (strncmp(propNamePtr, "_rdkit_", 7) == 0) {
       return propName.substr(8);
     }
   }
@@ -284,7 +285,8 @@ void set_atom_properties(Atom &atom, const mae::IndexedBlock &atom_block,
         prop.first == PDB_CHAIN_NAME || prop.first == PDB_INSERTION_CODE) {
       // PDB information is parsed separately.
       continue;
-    } else if (!prop.second->isDefined(i)) {
+    }
+    if (!prop.second->isDefined(i)) {
       continue;
     }
 
@@ -298,7 +300,8 @@ void set_atom_properties(Atom &atom, const mae::IndexedBlock &atom_block,
       // Coordinates are used in defining a conformation, and should not be
       // set on the atom.
       continue;
-    } else if (prop.first == PDB_OCCUPANCY || prop.first == PDB_TFACTOR) {
+    }
+    if (prop.first == PDB_OCCUPANCY || prop.first == PDB_TFACTOR) {
       // PDB information is parsed separately.
       continue;
     } else if (!prop.second->isDefined(i)) {
@@ -312,7 +315,8 @@ void set_atom_properties(Atom &atom, const mae::IndexedBlock &atom_block,
     if (prop.first == mae::ATOM_ATOMIC_NUM) {
       // Atomic number was already used in the creation of the atom
       continue;
-    } else if (prop.first == PDB_RESIDUE_NUMBER) {
+    }
+    if (prop.first == PDB_RESIDUE_NUMBER) {
       // PDB information is parsed separately.
       continue;
     } else if (!prop.second->isDefined(i)) {
@@ -351,7 +355,7 @@ void addAtoms(const mae::IndexedBlock &atom_block, RWMol &mol,
 
   // atomic numbers, and x, y, and z coordinates
   const auto size = atomicNumbers->size();
-  auto conf = new RDKit::Conformer(size);
+  auto *conf = new RDKit::Conformer(size);
   conf->setId(0);
 
   PDBInfo pdb_info(atom_block);
@@ -414,7 +418,7 @@ void addBonds(const mae::IndexedBlock &bond_block, RWMol &mol) {
     const auto from_atom = from_atoms->at(i) - 1;
     const auto to_atom = to_atoms->at(i) - 1;
     const auto order = bolookup.find(orders->at(i))->second;
-    if (auto bond = mol.getBondBetweenAtoms(from_atom, to_atom);
+    if (auto *bond = mol.getBondBetweenAtoms(from_atom, to_atom);
         bond != nullptr) {
       if (order != bond->getBondType()) {
         BOOST_LOG(rdWarningLog)
@@ -425,7 +429,7 @@ void addBonds(const mae::IndexedBlock &bond_block, RWMol &mol) {
       continue;  // Maestro files may double-list some bonds
     }
 
-    auto bond = new Bond(order);
+    auto *bond = new Bond(order);
     bond->setOwningMol(mol);
     bond->setBeginAtomIdx(from_atom);
     bond->setEndAtomIdx(to_atom);
@@ -577,7 +581,8 @@ std::unique_ptr<RWMol> MaeMolSupplier::next() {
   PRECONDITION(dp_sInStream != nullptr, "no stream");
   if (!d_stored_exc.empty()) {
     throw FileParseException(d_stored_exc);
-  } else if (atEnd()) {
+  }
+  if (atEnd()) {
     throw FileParseException("All structures read from Maestro file");
   }
 
