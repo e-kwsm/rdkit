ca1efa9527326b3e3e7ad4a1cc5830680ac29250
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-09 15:50:15 +0900

    Code/GraphMol/FileParsers/MultithreadedMolSupplier.cpp Code/GraphMol/FileParsers/MultithreadedMolSupplier.h Code/GraphMol/FileParsers/PNGParser.cpp Code/GraphMol/FileParsers/PNGParser.h Code/RDGeneral/ConcurrentQueue.h

--- Code/GraphMol/FileParsers/MultithreadedMolSupplier.h
+++ Code/GraphMol/FileParsers/MultithreadedMolSupplier.h
@@ -1,3 +1,6 @@
+#ifndef LLVM_CODE_GRAPHMOL_FILEPARSERS_MULTITHREADEDMOLSUPPLIER_H
+#define LLVM_CODE_GRAPHMOL_FILEPARSERS_MULTITHREADEDMOLSUPPLIER_H
+
 //
 //  Copyright (C) 2020 Shrey Aryan
 //
@@ -187,4 +190,6 @@ class RDKIT_FILEPARSERS_EXPORT MultithreadedMolSupplier : public MolSupplier {
 }  // namespace v2
 }  // namespace RDKit
 #endif
+#endif
+
 #endif
