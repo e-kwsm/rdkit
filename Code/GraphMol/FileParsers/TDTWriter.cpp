47589a07c1ce0b915bafde9e7e5291aed427e2c9
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-09 15:47:47 +0900

    Code/GraphMol/FileParsers/TDTMolSupplier.cpp Code/GraphMol/FileParsers/TDTWriter.cpp Code/GraphMol/FileParsers/TplFileParser.cpp

--- Code/GraphMol/FileParsers/TDTWriter.cpp
+++ Code/GraphMol/FileParsers/TDTWriter.cpp
@@ -9,8 +9,8 @@
 //  of the RDKit source tree.
 //
 #include <fstream>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <sstream>
 #include <string>
 
@@ -26,8 +26,8 @@
 #ifdef RDK_BUILD_MAEPARSER_SUPPORT
 #undef RDK_BUILD_MAEPARSER_SUPPORT
 #endif
-#include "MolWriters.h"
 #include "FileParsers.h"
+#include "MolWriters.h"
 
 namespace RDKit {
 TDTWriter::TDTWriter(const std::string &fileName) {
