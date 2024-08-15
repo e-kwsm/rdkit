9d4df21327f28363e174f3548817bfd6dd219c9d
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-15 23:47:50 +0900

    assertIsNone

--- Code/GraphMol/FMCS/Wrap/testFMCS.py
+++ Code/GraphMol/FMCS/Wrap/testFMCS.py
@@ -353,7 +353,7 @@ class Common:
       '[#6](:[#6]:[#6]):[#6]:[#7]:[#6]-[#6]-[#7](-[#6](-[#6])-[#6]1:[#6]:[#6]:[#6]:[#6]:[#7]:1)-[#6]-[#6]-[#6]-[#6]-[#7]'
     )
     qm = Chem.MolFromSmarts(mcs.smartsString)
-    self.assertTrue(qm is not None)
+    self.assertIsNotNone(qm)
     for m in ms:
       self.assertTrue(m.HasSubstructMatch(qm))
 
@@ -370,7 +370,7 @@ class Common:
       '[#6](:[#6]:[#6]):[#6]:[#7]:[#6]-[#6]-[#7](-[#6](-[#6])-[#6]1:[#6]:[#6]:[#6]:[#6]:[#7]:1)-[#6]-[#6]-[#6]-[#6]-[#7]'
     )
     qm = Chem.MolFromSmarts(mcs.smartsString)
-    self.assertTrue(qm is not None)
+    self.assertIsNotNone(qm)
     for m in ms:
       self.assertTrue(m.HasSubstructMatch(qm))
 
@@ -401,7 +401,7 @@ class Common:
     self.assertEqual(mcs.numBonds, 9)
     self.assertEqual(mcs.numAtoms, 10)
     qm = Chem.MolFromSmarts(mcs.smartsString)
-    self.assertTrue(qm is not None)
+    self.assertIsNotNone(qm)
     for m in ms:
       self.assertTrue(m.HasSubstructMatch(qm))
     # smarts too hard to canonicalize this
@@ -416,7 +416,7 @@ class Common:
     self.assertEqual(mcs.numBonds, 20)
     self.assertEqual(mcs.numAtoms, 19)
     qm = Chem.MolFromSmarts(mcs.smartsString)
-    self.assertTrue(qm is not None)
+    self.assertIsNotNone(qm)
     nHits = 0
     for m in ms:
       if m.HasSubstructMatch(qm):
