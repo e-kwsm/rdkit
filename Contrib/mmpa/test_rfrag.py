5f3a3a7ad99918e1a55057b277ec7c620eed1b08
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-16 04:31:44 +0900

    fixup! assertEqual

--- Contrib/mmpa/test_rfrag.py
+++ Contrib/mmpa/test_rfrag.py
@@ -15,7 +15,7 @@ class TestCase(unittest.TestCase):
   def test1Github1406(self):
     with open('data/simple.smi') as inf:
       p = subprocess.run(('python', 'rfrag.py'), stdin=inf, stdout=subprocess.PIPE)
-    self.assertFalse(p.returncode)
+    self.assertEqual(p.returncode, 0)
     self.assertEqual(p.stdout, b'''c1ccccc1,benzene,,
 Cc1ccccc1,toluene,,C[*:1].c1ccc(cc1)[*:1]
 ''')
