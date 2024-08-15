eb1e52348d619252cbcac70683ceda2e984f057b
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-16 02:04:30 +0900

    fixup! assertFalse

--- rdkit/Dbase/UnitTestStorageUtils.py
+++ rdkit/Dbase/UnitTestStorageUtils.py
@@ -43,7 +43,7 @@ class TestCase(unittest.TestCase):
     self.assertFalse(failed)
 
   def test_ValidateRDId(self):
-    self.assertEqual(StorageUtils.ValidateRDId('RDCmpd-bogus-000-9'), False)
+    self.assertFalse(StorageUtils.ValidateRDId('RDCmpd-bogus-000-9'))
 
 
 if __name__ == '__main__':  # pragma: nocover
