b910e33adf10fefa27312c6d77897090db320411
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-21 01:36:32 +0900

    refactor: forward declaration

--- Code/GraphMol/FileParsers/MolWriters.h
+++ Code/GraphMol/FileParsers/MolWriters.h
@@ -28,9 +28,8 @@ class Writer;
 }  // namespace schrodinger
 #endif  // RDK_BUILD_MAEPARSER_SUPPORT
 
-#include <GraphMol/ROMol.h>
-
 namespace RDKit {
+class ROMol;
 
 static int defaultConfId = -1;
 
