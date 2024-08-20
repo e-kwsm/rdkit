a855f77837a52ebe7fbac5fea4e0915e1567cf9d
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-21 01:36:32 +0900

    refactor: forward declaration

--- Code/GraphMol/FileParsers/MolSupplier.h
+++ Code/GraphMol/FileParsers/MolSupplier.h
@@ -20,7 +20,6 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-#include <GraphMol/ROMol.h>
 #include <RDGeneral/BadFileException.h>
 #include "FileParsers.h"
 #include <GraphMol/SmilesParse/SmilesParse.h>
