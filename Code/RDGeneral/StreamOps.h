3391e5ff960e446ae8d46b76b664a8cd09ef2875
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-09-18 10:45:27 +0900

    Code/DataStructs/BitVect.cpp Code/DataStructs/Utils.cpp Code/RDGeneral/Dict.h Code/RDGeneral/Invariant.h Code/RDGeneral/RDProps.h Code/RDGeneral/RDValue-taggedunion.h Code/RDGeneral/StreamOps.h

--- Code/RDGeneral/StreamOps.h
+++ Code/RDGeneral/StreamOps.h
@@ -161,7 +161,7 @@ inline boost::uint32_t readPackedIntFromStream(std::stringstream &ss) {
   if ((val & 1) == 0) {
     shift = 1;
   } else if ((val & 3) == 1) {
-    ss.read((char *)&tmp, sizeof(tmp));
+    ss.read((&tmp), sizeof(tmp));
     if (ss.fail()) {
       throw std::runtime_error("failed to read from stream");
     }
@@ -170,13 +170,13 @@ inline boost::uint32_t readPackedIntFromStream(std::stringstream &ss) {
     shift = 2;
     offset = (1 << 7);
   } else if ((val & 7) == 3) {
-    ss.read((char *)&tmp, sizeof(tmp));
+    ss.read((&tmp), sizeof(tmp));
     if (ss.fail()) {
       throw std::runtime_error("failed to read from stream");
     }
 
     val |= (UCHAR(tmp) << 8);
-    ss.read((char *)&tmp, sizeof(tmp));
+    ss.read((&tmp), sizeof(tmp));
     if (ss.fail()) {
       throw std::runtime_error("failed to read from stream");
     }
@@ -185,19 +185,19 @@ inline boost::uint32_t readPackedIntFromStream(std::stringstream &ss) {
     shift = 3;
     offset = (1 << 7) + (1 << 14);
   } else {
-    ss.read((char *)&tmp, sizeof(tmp));
+    ss.read((&tmp), sizeof(tmp));
     if (ss.fail()) {
       throw std::runtime_error("failed to read from stream");
     }
 
     val |= (UCHAR(tmp) << 8);
-    ss.read((char *)&tmp, sizeof(tmp));
+    ss.read((&tmp), sizeof(tmp));
     if (ss.fail()) {
       throw std::runtime_error("failed to read from stream");
     }
 
     val |= (UCHAR(tmp) << 16);
-    ss.read((char *)&tmp, sizeof(tmp));
+    ss.read((&tmp), sizeof(tmp));
     if (ss.fail()) {
       throw std::runtime_error("failed to read from stream");
     }
