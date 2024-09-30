af90584ea90feeda81a5e4beae92febdc98c1484
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-01 02:03:17 +0900

    Code/GraphMol/FileParsers/Mol2FileParser.cpp Code/GraphMol/FileParsers/MolFileParser.cpp Code/GraphMol/SmilesParse/catch_tests.cpp

--- Code/GraphMol/SmilesParse/catch_tests.cpp
+++ Code/GraphMol/SmilesParse/catch_tests.cpp
@@ -2002,7 +2002,7 @@ M  END)CTAB"_ctab;
     REQUIRE(m);
     m->clearConformers();
     auto smi = MolToCXSmiles(*m);
-    CHECK(smi.find("$") == std::string::npos);
+    CHECK(smi.find('$') == std::string::npos);
   }
 }
 
