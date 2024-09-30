5680887bbdde5dccb7ff7b33a303477e4b868d1e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-01 01:57:05 +0900

    perf: performance-move-const-arg

--- Code/GraphMol/ROMol.h
+++ Code/GraphMol/ROMol.h
@@ -431,7 +431,7 @@ class RDKIT_GRAPHMOL_EXPORT ROMol : public RDProps {
 
   ROMol(ROMol &&o) noexcept
       : RDProps(std::move(o)),
-        d_graph(std::move(o.d_graph)),
+        d_graph(o.d_graph),
         d_atomBookmarks(std::move(o.d_atomBookmarks)),
         d_bondBookmarks(std::move(o.d_bondBookmarks)),
         d_confs(std::move(o.d_confs)),
@@ -461,7 +461,7 @@ class RDKIT_GRAPHMOL_EXPORT ROMol : public RDProps {
       return *this;
     }
     RDProps::operator=(std::move(o));
-    d_graph = std::move(o.d_graph);
+    d_graph = o.d_graph;
     d_atomBookmarks = std::move(o.d_atomBookmarks);
     d_bondBookmarks = std::move(o.d_bondBookmarks);
     if (dp_ringInfo) {
