2fb942824e2110a5c51987d8403dfe1bedd52223
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-01 02:04:48 +0900

    Code/GraphMol/MolOps.cpp Code/GraphMol/ROMol.cpp Code/GraphMol/RWMol.cpp

--- Code/GraphMol/ROMol.cpp
+++ Code/GraphMol/ROMol.cpp
@@ -637,7 +637,7 @@ const Conformer &ROMol::getConformer(int id) const {
     return *(d_confs.front());
   }
   auto cid = (unsigned int)id;
-  for (auto conf : d_confs) {
+  for (const auto &conf : d_confs) {
     if (conf->getId() == cid) {
       return *conf;
     }
