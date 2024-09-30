7b733a1174288470c3e4d4d911eb13c98ed8a77a
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-01 02:04:48 +0900

    Code/GraphMol/MolOps.cpp Code/GraphMol/ROMol.cpp Code/GraphMol/RWMol.cpp

--- Code/GraphMol/RWMol.cpp
+++ Code/GraphMol/RWMol.cpp
@@ -892,7 +892,7 @@ void RWMol::batchRemoveAtoms() {
   }
 
   // do the same with the coordinates in the conformations
-  for (auto conf : d_confs) {
+  for (const auto &conf : d_confs) {
     RDGeom::POINT3D_VECT &positions = conf->getPositions();
     RDGeom::POINT3D_VECT newPositions;
     newPositions.reserve(getNumAtoms());
