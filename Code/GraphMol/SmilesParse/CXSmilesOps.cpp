b9d8bbacc06ff8f52c90d0d1f1ece5d38e827ec2
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-01 02:09:50 +0900

    Code/GraphMol/SmilesParse/CXSmilesOps.cpp

--- Code/GraphMol/SmilesParse/CXSmilesOps.cpp
+++ Code/GraphMol/SmilesParse/CXSmilesOps.cpp
@@ -1036,7 +1036,7 @@ bool parse_variable_attachments(Iterator &first, Iterator last,
     }
     if (VALID_ATIDX(at1idx)) {
       std::string endPts = "(" + std::to_string(others.size());
-      for (auto idx : others) {
+      for (const auto &idx : others) {
         endPts += " " + idx;
       }
       endPts += ")";
@@ -2246,7 +2246,7 @@ std::string get_bond_config_block(
   }
   std::string res = "";
 
-  for (auto wPart : wParts) {
+  for (const auto &wPart : wParts) {
     if (res != "") {
       res += ",";
     }
