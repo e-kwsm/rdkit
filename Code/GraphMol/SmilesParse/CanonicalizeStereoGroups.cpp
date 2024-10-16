4ac3f654ee2421c0ed9532df898aa5093625b671
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-16 18:47:38 +0900

    Code/GraphMol/SmilesParse/CanonicalizeStereoGroups.cpp

--- Code/GraphMol/SmilesParse/CanonicalizeStereoGroups.cpp
+++ Code/GraphMol/SmilesParse/CanonicalizeStereoGroups.cpp
@@ -630,7 +630,7 @@ void canonicalizeStereoGroups_internal(
     boost::dynamic_bitset<> atomIndicesInStereoGroups(newMol->getNumAtoms());
     boost::dynamic_bitset<> bondIndicesInStereoGroups(newMol->getNumBonds());
 
-    for (auto grp : newMol->getStereoGroups()) {
+    for (const auto &grp : newMol->getStereoGroups()) {
       for (auto atomPtr : grp.getAtoms()) {
         atomIndicesInStereoGroups.set(atomPtr->getIdx());
       }
@@ -831,7 +831,7 @@ void canonicalizeStereoGroups_internal(
   // routine for the other kind of stereo groups.
 
   if (!bestNewMol->getStereoGroups().empty()) {
-    for (auto grp : bestNewMol->getStereoGroups()) {
+    for (const auto &grp : bestNewMol->getStereoGroups()) {
       newGroups.push_back(grp);
     }
   }
