dfc6506f6f768b20f84262d93d95326246e7f3b9
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-31 14:13:01 +0900

    Code/GraphMol/ForceFieldHelpers/CrystalFF/testCrystalFF.cpp Code/GraphMol/ForceFieldHelpers/MMFF/testMMFFHelpers.cpp Code/GraphMol/ForceFieldHelpers/UFF/testUFFHelpers.cpp

--- Code/GraphMol/ForceFieldHelpers/CrystalFF/testCrystalFF.cpp
+++ Code/GraphMol/ForceFieldHelpers/CrystalFF/testCrystalFF.cpp
@@ -31,7 +31,10 @@ void testTorsionAngleContribs() {
   std::cerr << " Test CrystalFF torsional term." << std::endl;
 
   ForceFields::ForceField ff;
-  Point3D p1, p2, p3, p4;
+  Point3D p1;
+  Point3D p2;
+  Point3D p3;
+  Point3D p4;
   RDGeom::PointPtrVect &ps = ff.positions();
   ps.push_back(&p1);
   ps.push_back(&p2);
