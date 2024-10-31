ca05e3f097bab70bc56ee4648388b2e71a2da089
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-31 14:13:01 +0900

    Code/GraphMol/ForceFieldHelpers/CrystalFF/testCrystalFF.cpp Code/GraphMol/ForceFieldHelpers/MMFF/testMMFFHelpers.cpp Code/GraphMol/ForceFieldHelpers/UFF/testUFFHelpers.cpp

--- Code/GraphMol/ForceFieldHelpers/UFF/testUFFHelpers.cpp
+++ Code/GraphMol/ForceFieldHelpers/UFF/testUFFHelpers.cpp
@@ -139,7 +139,8 @@ void testUFFTyper2() {
   BOOST_LOG(rdErrorLog) << "-------------------------------------" << std::endl;
   BOOST_LOG(rdErrorLog) << "    Test UFF atom typer." << std::endl;
 
-  ROMol *mol, *mol2;
+  ROMol *mol;
+  ROMol *mol2;
   std::string key;
 
   mol = SmilesToMol("[SiH3]CC(=O)NC");
@@ -209,7 +210,8 @@ void testUFFBuilder1() {
   BOOST_LOG(rdErrorLog) << "-------------------------------------" << std::endl;
   BOOST_LOG(rdErrorLog) << "    Testing UFF builder tools." << std::endl;
 
-  ROMol *mol, *mol2;
+  ROMol *mol;
+  ROMol *mol2;
   std::string key;
 
   UFF::AtomicParamVect types;
@@ -627,7 +629,8 @@ void testUFFBuilderSpecialCases() {
   RWMol *mol;
   std::string key;
   int needMore;
-  RDGeom::Point3D v1, v2;
+  RDGeom::Point3D v1;
+  RDGeom::Point3D v2;
   ForceFields::ForceField *field;
 
   std::string pathName = getenv("RDBASE");
@@ -687,7 +690,8 @@ void testIssue239() {
   int needMore;
   (void)needMore;  // add test later
   ForceFields::ForceField *field;
-  double e1, e2;
+  double e1;
+  double e2;
 
   std::string pathName = getenv("RDBASE");
   pathName += "/Code/GraphMol/ForceFieldHelpers/UFF/test_data";
@@ -717,7 +721,9 @@ void testCalcEnergyPassedCoords() {
 
   RWMol *mol;
   ForceFields::ForceField *field;
-  double e1, e2, e3;
+  double e1;
+  double e2;
+  double e3;
 
   std::string pathName = getenv("RDBASE");
   pathName += "/Code/GraphMol/ForceFieldHelpers/MMFF/test_data";
