cf61480c17aed3d468a9738f5b64abd2517c36e2
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-31 14:12:17 +0900

    Code/GraphMol/MolTransforms/test1.cpp

--- Code/GraphMol/MolTransforms/test1.cpp
+++ Code/GraphMol/MolTransforms/test1.cpp
@@ -135,7 +135,8 @@ void test1Canonicalization() {
   canonicalizeConformer(conf1);
 
   Conformer &conf2 = mol2->getConformer();
-  unsigned int i, nats = mol->getNumAtoms();
+  unsigned int i;
+  unsigned int nats = mol->getNumAtoms();
   for (i = 0; i < nats; ++i) {
     CHECK_INVARIANT(comparePts(conf1.getAtomPos(i), conf2.getAtomPos(i)), "");
   }
@@ -258,7 +259,8 @@ void _calcAxesAndMoments(RWMol *m, Eigen::Matrix3d &axes,
     weights[(*cai)->getIdx()] = (*cai)->getMass();
   }
 
-  bool ignoreHs = false, force = true;
+  bool ignoreHs = false;
+  bool force = true;
   computePrincipalAxesAndMoments(conf, axes, moments, ignoreHs, force,
                                  &weights);
 }
