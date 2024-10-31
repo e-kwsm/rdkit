8b03a7be9c77658156c73ef46ada4b218f3e2f10
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-10-31 14:15:09 +0900

    Code/GraphMol/MolChemicalFeatures/testFeatures.cpp

--- Code/GraphMol/MolChemicalFeatures/testFeatures.cpp
+++ Code/GraphMol/MolChemicalFeatures/testFeatures.cpp
@@ -122,7 +122,8 @@ void test2() {
   BOOST_LOG(rdErrorLog) << "-------------------------------------" << std::endl;
   BOOST_LOG(rdErrorLog) << "FeatureDefinition parser testing." << std::endl;
 
-  std::string inText, inLine;
+  std::string inText;
+  std::string inLine;
   std::istringstream ss;
   MolChemicalFeatureDef *featDef;
   bool ok;
@@ -222,7 +223,8 @@ void test3() {
   // std::string pathName=getenv("RDBASE");
   // pathName +=
   // "/Code/GraphMol/MolChemicalFeatures/test_data/smallsample.feats";
-  std::string inText, inLine;
+  std::string inText;
+  std::string inLine;
   std::istringstream ss;
   int res;
   MolChemicalFeatureDef::CollectionType featureDefs;
@@ -480,7 +482,8 @@ void testIssue224() {
   BOOST_LOG(rdErrorLog) << "-------------------------------------" << std::endl;
   BOOST_LOG(rdErrorLog) << "Testing Issue 224." << std::endl;
 
-  std::string inText, inLine;
+  std::string inText;
+  std::string inLine;
   std::istringstream ss;
   MolChemicalFeatureDef *featDef;
 
