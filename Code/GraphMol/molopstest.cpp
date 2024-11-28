2fcc3a009d2b0b3387dc2b05e45e7416afd8e159
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-11-28 16:16:32 +0900

    Code/GraphMol/molopstest.cpp

--- Code/GraphMol/molopstest.cpp
+++ Code/GraphMol/molopstest.cpp
@@ -7120,7 +7120,7 @@ void testGithubIssue868() {
     sstrm.str("");
     TEST_ASSERT(sstrm.str() == "");
     RWMol m;
-    QueryAtom *qa = new QueryAtom();
+    auto *qa = new QueryAtom();
     qa->setQuery(makeAtomTypeQuery(1, aromatic));
     qa->expandQuery(makeAtomNumQuery(6), Queries::CompositeQueryType::COMPOSITE_OR);
     m.addAtom(qa, true, true);
