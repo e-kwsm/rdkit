01446d156837e929a2fcd30960501dd51589188a
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-13 19:26:51 +0900

    Code/GraphMol/AddHs.cpp

--- Code/GraphMol/AddHs.cpp
+++ Code/GraphMol/AddHs.cpp
@@ -1210,9 +1210,8 @@ HydrogenType isQueryH(const Atom *atom) {
                "be merged"
             << std::endl;
         return HydrogenType::UnMergableQueryHydrogen;
-      } else {
-        return HydrogenType::QueryHydrogen;
       }
+        return HydrogenType::QueryHydrogen;
     }
   }
   return HydrogenType::NotAHydrogen;
