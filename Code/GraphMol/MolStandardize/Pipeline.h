951d57bd96329cc835bf35a03bc8644136421136
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-27 18:38:34 +0900

    Code/GraphMol/MolStandardize/Pipeline.h

--- Code/GraphMol/MolStandardize/Pipeline.h
+++ Code/GraphMol/MolStandardize/Pipeline.h
@@ -210,7 +210,7 @@ class RDKIT_MOLSTANDARDIZE_EXPORT Pipeline {
 
  public:
   Pipeline() = default;
-  explicit Pipeline(const PipelineOptions &o) : options(o) {};
+  explicit Pipeline(PipelineOptions o) : options(std::move(o)) {}
   ~Pipeline() = default;
 
   PipelineResult run(const std::string &molblock) const;
