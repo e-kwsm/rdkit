7607ca617c35a2c660eb6b886e781bdfac8f5e58
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-27 19:41:36 +0900

    Code/Geometry/Wrap/UniformRealValueGrid3D.cpp Code/GraphMol/FMCS/Test/testFMCS.cpp Code/GraphMol/FileParsers/testAtropisomers.cpp Code/GraphMol/MarvinParse/testMrvToMol.cpp Code/GraphMol/MolDraw2D/test1.cpp Code/GraphMol/MolInteractionFields/MIFDescriptors.h Code/GraphMol/RGroupDecomposition/testRGroupDecomp.cpp Code/GraphMol/molopstest.cpp Code/RDBoost/Wrap/RDBase.cpp Code/RDGeneral/RDAny.h Code/RDGeneral/testConcurrentQueue.cpp Contrib/ConformerParser/ConformerParser.cpp

--- Code/GraphMol/MarvinParse/testMrvToMol.cpp
+++ Code/GraphMol/MarvinParse/testMrvToMol.cpp
@@ -27,6 +27,7 @@
 #include <string>
 #include <fstream>
 #include <filesystem>
+#include <utility>
 
 using namespace RDKit;
 using namespace v2::FileParsers;
@@ -57,7 +58,7 @@ class MrvTests {
             bool reapplyMolBlockWedgingInit = true)
         : atomCount(atomCountInit),
           bondCount(bondCountInit),
-          fileName(fileNameInit),
+          fileName(std::move(fileNameInit)),
           expectedResult(expectedResultInit),
           sanitizeFlag(sanitizeFlagInit),
           reapplyMolBlockWedging(reapplyMolBlockWedgingInit) {};
@@ -96,7 +97,7 @@ class MrvTests {
     RxnTest(std::string fileNameInit, bool expectedResultInit,
             int reactantCountInit, int agentCountInit, int productCountInit,
             int warnInit, int errorInit)
-        : fileName(fileNameInit),
+        : fileName(std::move(fileNameInit)),
           expectedResult(expectedResultInit),
           reactantCount(reactantCountInit),
           agentCount(agentCountInit),
@@ -117,8 +118,8 @@ class MrvTests {
     SmilesTest(std::string nameInit, std::string smilesInit,
                bool expectedResultInit, int atomCountInit, int bondCountInit,
                bool sanitizeFlagInit)
-        : name(nameInit),
-          smiles(smilesInit),
+        : name(std::move(nameInit)),
+          smiles(std::move(smilesInit)),
           expectedResult(expectedResultInit),
           sanitizeFlag(sanitizeFlagInit),
           atomCount(atomCountInit),
@@ -126,8 +127,8 @@ class MrvTests {
 
     SmilesTest(std::string nameInit, std::string smilesInit,
                bool expectedResultInit, int atomCountInit, int bondCountInit)
-        : name(nameInit),
-          smiles(smilesInit),
+        : name(std::move(nameInit)),
+          smiles(std::move(smilesInit)),
           expectedResult(expectedResultInit),
           sanitizeFlag(true),
           atomCount(atomCountInit),
