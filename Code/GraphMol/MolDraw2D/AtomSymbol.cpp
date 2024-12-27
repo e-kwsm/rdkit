1e10a740029bdb7a4c52f2b45a7a5cb99f858b5a
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-27 18:57:55 +0900

    Code/GraphMol/MolDraw2D/AtomSymbol.cpp Code/GraphMol/MolDraw2D/AtomSymbol.h Code/GraphMol/MolDraw2D/DrawAnnotation.cpp Code/GraphMol/MolDraw2D/DrawAnnotation.h Code/GraphMol/MolDraw2D/DrawMol.cpp Code/GraphMol/MolDraw2D/DrawMol.h Code/GraphMol/MolDraw2D/DrawMolMCHCircleAndLine.cpp Code/GraphMol/MolDraw2D/DrawMolMCHLasso.cpp Code/GraphMol/MolDraw2D/DrawShape.cpp Code/GraphMol/MolDraw2D/DrawShape.h Code/GraphMol/MolDraw2D/DrawText.cpp Code/GraphMol/MolDraw2D/DrawTextFT.h Code/GraphMol/MolDraw2D/DrawTextNotFT.h Code/GraphMol/MolDraw2D/MolDraw2D.cpp Code/GraphMol/RascalMCES/RascalResult.cpp Code/GraphMol/RascalMCES/RascalResult.h Code/GraphMol/SubstructLibrary/SubstructLibrary.cpp Code/GraphMol/SubstructLibrary/SubstructLibrarySerialization.h

--- Code/GraphMol/MolDraw2D/AtomSymbol.cpp
+++ Code/GraphMol/MolDraw2D/AtomSymbol.cpp
@@ -24,10 +24,10 @@ namespace RDKit {
 namespace MolDraw2D_detail {
 
 // ****************************************************************************
-AtomSymbol::AtomSymbol(const std::string &symbol, int atIdx, OrientType orient,
+AtomSymbol::AtomSymbol(std::string symbol, int atIdx, OrientType orient,
                        const Point2D &cds, const DrawColour &colour,
                        DrawText &textDrawer)
-    : symbol_(symbol),
+    : symbol_(std::move(symbol)),
       atIdx_(atIdx),
       orient_(orient),
       cds_(cds),
