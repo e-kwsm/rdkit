5410d472767fcbe8090ac04120350683889056c3
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-01-04 03:52:22 +0900

    Code/Geometry/Wrap/UniformRealValueGrid3D.cpp Code/Geometry/catch_tests.cpp Code/Geometry/testRealValueGrid.cpp Code/GraphMol/MolInteractionFields/MIFDescriptors.cpp Code/GraphMol/SmilesParse/catch_tests.cpp Code/GraphMol/catch_organometallics.cpp

--- Code/Geometry/testRealValueGrid.cpp
+++ Code/Geometry/testRealValueGrid.cpp
@@ -65,45 +65,65 @@ void test2UniformRealValueGrid3DPickling() {
 void test3UniformRealValueGrid3DIndexing() {
   RDGeom::UniformRealValueGrid3D grd(5.0, 5.0, 5.0, 0.1);
   {
-    unsigned int xi = 3, yi = 3, zi = 3;
+    unsigned int xi = 3;
+    unsigned int yi = 3;
+    unsigned int zi = 3;
     unsigned int idx = grd.getGridIndex(xi, yi, zi);
-    unsigned int nxi, nyi, nzi;
+    unsigned int nxi;
+    unsigned int nyi;
+    unsigned int nzi;
     grd.getGridIndices(idx, nxi, nyi, nzi);
     TEST_ASSERT(nxi == xi);
     TEST_ASSERT(nyi == yi);
     TEST_ASSERT(nzi == zi);
   }
   {
-    unsigned int xi = 3, yi = 3, zi = 5;
+    unsigned int xi = 3;
+    unsigned int yi = 3;
+    unsigned int zi = 5;
     unsigned int idx = grd.getGridIndex(xi, yi, zi);
-    unsigned int nxi, nyi, nzi;
+    unsigned int nxi;
+    unsigned int nyi;
+    unsigned int nzi;
     grd.getGridIndices(idx, nxi, nyi, nzi);
     TEST_ASSERT(nxi == xi);
     TEST_ASSERT(nyi == yi);
     TEST_ASSERT(nzi == zi);
   }
   {
-    unsigned int xi = 3, yi = 6, zi = 3;
+    unsigned int xi = 3;
+    unsigned int yi = 6;
+    unsigned int zi = 3;
     unsigned int idx = grd.getGridIndex(xi, yi, zi);
-    unsigned int nxi, nyi, nzi;
+    unsigned int nxi;
+    unsigned int nyi;
+    unsigned int nzi;
     grd.getGridIndices(idx, nxi, nyi, nzi);
     TEST_ASSERT(nxi == xi);
     TEST_ASSERT(nyi == yi);
     TEST_ASSERT(nzi == zi);
   }
   {
-    unsigned int xi = 0, yi = 0, zi = 0;
+    unsigned int xi = 0;
+    unsigned int yi = 0;
+    unsigned int zi = 0;
     unsigned int idx = grd.getGridIndex(xi, yi, zi);
-    unsigned int nxi, nyi, nzi;
+    unsigned int nxi;
+    unsigned int nyi;
+    unsigned int nzi;
     grd.getGridIndices(idx, nxi, nyi, nzi);
     TEST_ASSERT(nxi == xi);
     TEST_ASSERT(nyi == yi);
     TEST_ASSERT(nzi == zi);
   }
   {
-    unsigned int xi = 8, yi = 2, zi = 1;
+    unsigned int xi = 8;
+    unsigned int yi = 2;
+    unsigned int zi = 1;
     unsigned int idx = grd.getGridIndex(xi, yi, zi);
-    unsigned int nxi, nyi, nzi;
+    unsigned int nxi;
+    unsigned int nyi;
+    unsigned int nzi;
     grd.getGridIndices(idx, nxi, nyi, nzi);
     TEST_ASSERT(nxi == xi);
     TEST_ASSERT(nyi == yi);
