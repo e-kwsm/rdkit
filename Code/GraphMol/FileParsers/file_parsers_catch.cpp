0b58ca6ed753126e519403afe9036a617b10925b
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-01-04 14:59:35 +0900

    Code/GraphMol/FileParsers/file_parsers_catch.cpp Code/GraphMol/ForceFieldHelpers/FFConvenience.h Code/RDGeneral/testDict.cpp

--- Code/GraphMol/FileParsers/file_parsers_catch.cpp
+++ Code/GraphMol/FileParsers/file_parsers_catch.cpp
@@ -2758,7 +2758,8 @@ TEST_CASE("multiple molecules in the PNG", "[writer][PNG]") {
 
   std::vector<std::string> smiles = {"c1ccccc1", "CCCOC", "c1ncccc1"};
   std::vector<std::unique_ptr<ROMol>> mols;
-  for (const auto &smi : smiles) {
+  mols.reserve(smiles.size());
+for (const auto &smi : smiles) {
     mols.emplace_back(SmilesToMol(smi));
   }
   SECTION("pickles") {
@@ -2811,7 +2812,8 @@ TEST_CASE("multiple molecules in the PNG, second example", "[writer][PNG]") {
 
   std::vector<std::string> smiles = {"c1ccccc1", "CCO", "CC(=O)O", "c1ccccn1"};
   std::vector<std::unique_ptr<ROMol>> mols;
-  for (const auto &smi : smiles) {
+  mols.reserve(smiles.size());
+for (const auto &smi : smiles) {
     mols.emplace_back(SmilesToMol(smi));
   }
 #ifdef RDK_USE_BOOST_IOSTREAMS
