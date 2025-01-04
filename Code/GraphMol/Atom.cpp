59a84cfddc598e72e80348abc991f644f1d86e5d
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-01-04 14:12:51 +0900

    XXX	Code/

--- Code/GraphMol/Atom.cpp
+++ Code/GraphMol/Atom.cpp
@@ -442,7 +442,7 @@ int calculateExplicitValence(const Atom &atom, bool strict, bool checkIt) {
                << periodicTable->getElementSymbol(atom.getAtomicNum())
                << ", " << res << ", is greater than permitted";
         std::string msg = errout.str();
-        BOOST_LOG(rdErrorLog) << msg << std::endl;
+        BOOST_LOG(rdErrorLog) << msg << '\n';
         throw AtomValenceException(msg, atom.getIdx());
       } else {
         return -1;
@@ -488,7 +488,7 @@ int calculateImplicitValence(const Atom &atom, bool strict, bool checkIt) {
         errout << "Unreasonable formal charge on atom # " << atom.getIdx()
                << ".";
         std::string msg = errout.str();
-        BOOST_LOG(rdErrorLog) << msg << std::endl;
+        BOOST_LOG(rdErrorLog) << msg << '\n';
         throw AtomValenceException(msg, atom.getIdx());
       } else if (checkIt) {
         return -1;
@@ -572,7 +572,7 @@ int calculateImplicitValence(const Atom &atom, bool strict, bool checkIt) {
           errout << "Explicit valence for aromatic atom # " << atom.getIdx()
                  << " not equal to any accepted valence\n";
           std::string msg = errout.str();
-          BOOST_LOG(rdErrorLog) << msg << std::endl;
+          BOOST_LOG(rdErrorLog) << msg << '\n';
           throw AtomValenceException(msg, atom.getIdx());
         } else {
           return -1;
@@ -602,7 +602,7 @@ int calculateImplicitValence(const Atom &atom, bool strict, bool checkIt) {
                  << PeriodicTable::getTable()->getElementSymbol(atomicNum)
                  << " greater than permitted";
           std::string msg = errout.str();
-          BOOST_LOG(rdErrorLog) << msg << std::endl;
+          BOOST_LOG(rdErrorLog) << msg << '\n';
           throw AtomValenceException(msg, atom.getIdx());
         } else {
           return -1;
