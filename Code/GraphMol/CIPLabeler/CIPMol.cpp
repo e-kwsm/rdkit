723eb36da4681dd802e981b9c96136250e2141e1
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-01-04 14:33:27 +0900

    XXX

--- Code/GraphMol/CIPLabeler/CIPMol.cpp
+++ Code/GraphMol/CIPLabeler/CIPMol.cpp
@@ -90,7 +90,7 @@ int CIPMol::getBondOrder(Bond *bond) const {
     case Bond::AROMATIC:
       BOOST_LOG(rdWarningLog)
           << "non kekulizable aromatic bond being treated as bond order 1"
-          << std::endl;
+          << '\n';
       return 1;
     case Bond::DOUBLE:
       return 2;
