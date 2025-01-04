23362d6aef64a3a58a644e384b54b6338a6e53aa
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-01-04 14:12:51 +0900

    XXX	Code/

--- Code/DataStructs/FPBReader.cpp
+++ Code/DataStructs/FPBReader.cpp
@@ -609,7 +609,7 @@ void FPBReader::init() {
         // currently ignored
       } else {
         BOOST_LOG(rdWarningLog)
-            << "Unknown chunk: " << chunkNm << " ignored." << std::endl;
+            << "Unknown chunk: " << chunkNm << " ignored." << '\n';
       }
       delete[] chunk;
     } else {
