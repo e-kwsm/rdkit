0c0506b6f9ef14ba29a34d15008380134da9fd07
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-03-26 19:28:48 +0900

    fixup! -Werror=extra-semi-stmt

--- Code/GraphMol/MolOps.h
+++ Code/GraphMol/MolOps.h
@@ -1095,11 +1095,11 @@ class Hybridizations {
  public:
   Hybridizations() {
     throw FileParseException("not to be called without a mol parameter");
-  };
+  }
   Hybridizations(const ROMol &mol);
   Hybridizations(const Hybridizations &) {
     throw FileParseException("not to be called without a mol parameter");
-  };
+  }
 
   ~Hybridizations() = default;
 
