dcf5b06a5648b70235d340ad17b496676dfbc415
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-03-26 19:28:48 +0900

    fixup! -Werror=extra-semi-stmt

--- Code/GraphMol/Chirality.h
+++ Code/GraphMol/Chirality.h
@@ -244,8 +244,8 @@ enum class WedgeInfoType {
 
 class WedgeInfoBase {
  public:
-  WedgeInfoBase(int idxInit) : idx(idxInit) {};
-  virtual ~WedgeInfoBase() {};
+  WedgeInfoBase(int idxInit) : idx(idxInit) {}
+  virtual ~WedgeInfoBase() {}
 
   virtual WedgeInfoType getType() const = 0;
   virtual Bond::BondDir getDir() const = 0;
@@ -258,8 +258,8 @@ class WedgeInfoBase {
 
 class WedgeInfoChiral : public WedgeInfoBase {
  public:
-  WedgeInfoChiral(int atomId) : WedgeInfoBase(atomId) {};
-  ~WedgeInfoChiral() {};
+  WedgeInfoChiral(int atomId) : WedgeInfoBase(atomId) {}
+  ~WedgeInfoChiral() {}
 
   WedgeInfoType getType() const override {
     return Chirality::WedgeInfoType::WedgeInfoTypeChiral;
@@ -275,8 +275,8 @@ class WedgeInfoAtropisomer : public WedgeInfoBase {
   WedgeInfoAtropisomer(int bondId, RDKit::Bond::BondDir dirInit)
       : WedgeInfoBase(bondId) {
     dir = dirInit;
-  };
-  ~WedgeInfoAtropisomer() {};
+  }
+  ~WedgeInfoAtropisomer() {}
 
   RDKit::Bond::BondDir dir = RDKit::Bond::BondDir::NONE;
 
