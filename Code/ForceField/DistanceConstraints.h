3489924f14513bb5374492ac8bd8ce5f03d632d9
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-03-26 19:43:36 +0900

    Code/ForceField/DistanceConstraints.h Code/ForceField/ForceField.cpp

--- Code/ForceField/DistanceConstraints.h
+++ Code/ForceField/DistanceConstraints.h
@@ -29,7 +29,7 @@ struct DistanceConstraintContribsParams {
         idx2(idx2),
         minLen(minLen),
         maxLen(maxLen),
-        forceConstant(forceConstant) {};
+        forceConstant(forceConstant) {}
 };
 
 //! A term to capture all flat bottom distance constraint potentials.
