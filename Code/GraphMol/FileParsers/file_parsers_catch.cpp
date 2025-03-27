d0448b977f28be5a55322939263f36c6af9fe8ae
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-03-27 09:57:58 +0900

    Code/RDGeneral/Dict.h Code/RDGeneral/StreamOps.h

--- Code/GraphMol/FileParsers/file_parsers_catch.cpp
+++ Code/GraphMol/FileParsers/file_parsers_catch.cpp
@@ -3275,7 +3275,7 @@ TEST_CASE("test output with incomplete monomer info", "[bug][writer]") {
     {
       auto m = "Cl"_smiles;
       // will get deleted by ~Atom()
-      AtomPDBResidueInfo *info = new AtomPDBResidueInfo();
+      auto *info = new AtomPDBResidueInfo();
       info->setResidueName("HCL");
       m->getAtomWithIdx(0)->setMonomerInfo(info);
       std::string pdb = MolToPDBBlock(*m, -1);
@@ -3284,7 +3284,7 @@ TEST_CASE("test output with incomplete monomer info", "[bug][writer]") {
     {
       auto m = "Cl"_smiles;
       // will get deleted by ~Atom()
-      AtomPDBResidueInfo *info = new AtomPDBResidueInfo();
+      auto *info = new AtomPDBResidueInfo();
       info->setResidueName("HCL");
       info->setName("Cl1");
       m->getAtomWithIdx(0)->setMonomerInfo(info);
@@ -3296,7 +3296,7 @@ TEST_CASE("test output with incomplete monomer info", "[bug][writer]") {
       // 2. should add spaces for missing residue name.
       auto m = "Cl"_smiles;
       // will get deleted by ~Atom()
-      AtomPDBResidueInfo *info = new AtomPDBResidueInfo();
+      auto *info = new AtomPDBResidueInfo();
       info->setName("CL");
       m->getAtomWithIdx(0)->setMonomerInfo(info);
       std::string pdb = MolToPDBBlock(*m, -1);
@@ -3307,7 +3307,7 @@ TEST_CASE("test output with incomplete monomer info", "[bug][writer]") {
       // 2. Test that residue names get truncated to 3 letters.
       auto m = "Cl"_smiles;
       // will get deleted by ~Atom()
-      AtomPDBResidueInfo *info = new AtomPDBResidueInfo();
+      auto *info = new AtomPDBResidueInfo();
       info->setName("CHLORINE_ATOM1");
       info->setResidueName("CHLORINE_MOLECULE1");
       m->getAtomWithIdx(0)->setMonomerInfo(info);
--- Code/GraphMol/molopstest.cpp
+++ Code/GraphMol/molopstest.cpp
@@ -1868,7 +1868,7 @@ void testShortestPath() {
 
     INT_LIST path = MolOps::getShortestPath(*m, 1, 20);
     CHECK_INVARIANT(path.size() == 7, "");
-    INT_LIST_CI pi = path.begin();
+    auto pi = path.begin();
     CHECK_INVARIANT((*pi) == 1, "");
     pi++;
     CHECK_INVARIANT((*pi) == 2, "");
@@ -1893,7 +1893,7 @@ void testShortestPath() {
     INT_LIST path = MolOps::getShortestPath(*m, 0, 1);
     std::cerr << "path: " << path.size() << std::endl;
     CHECK_INVARIANT(path.size() == 2, "");
-    INT_LIST_CI pi = path.begin();
+    auto pi = path.begin();
     CHECK_INVARIANT((*pi) == 0, "");
     pi++;
     CHECK_INVARIANT((*pi) == 1, "");
@@ -1912,7 +1912,7 @@ void testShortestPath() {
 
     INT_LIST path = MolOps::getShortestPath(*m, 8, 11);
     CHECK_INVARIANT(path.size() == 7, "");
-    INT_LIST_CI pi = path.begin();
+    auto pi = path.begin();
     CHECK_INVARIANT((*pi) == 8, "");
     pi++;
     CHECK_INVARIANT((*pi) == 7, "");
@@ -4185,8 +4185,7 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
+    for (auto mvit = mv.begin(); mvit != mv.end(); ++mvit) {
       mmap[mvit->second] = mvit->first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[2], mmap[3])->getBondType() ==
@@ -4222,8 +4221,7 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
+    for (auto mvit = mv.begin(); mvit != mv.end(); ++mvit) {
       mmap[mvit->second] = mvit->first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[10], mmap[11])->getBondType() ==
@@ -4352,8 +4350,7 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
+    for (auto mvit = mv.begin(); mvit != mv.end(); ++mvit) {
       mmap[mvit->second] = mvit->first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[1], mmap[2])->getBondType() ==
@@ -4381,8 +4378,7 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
+    for (auto mvit = mv.begin(); mvit != mv.end(); ++mvit) {
       mmap[mvit->second] = mvit->first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[4], mmap[5])->getBondType() ==
@@ -4428,8 +4424,7 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
+    for (auto mvit = mv.begin(); mvit != mv.end(); ++mvit) {
       mmap[mvit->second] = mvit->first;
     }
 
@@ -4500,8 +4495,7 @@ void testBasicCanon() {
     MatchVectType mv;
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
+    for (auto mvit = mv.begin(); mvit != mv.end(); ++mvit) {
       mmap[mvit->second] = mvit->first;
     }
 
@@ -4526,8 +4520,7 @@ void testBasicCanon() {
     m2 = SmilesToMol(tsmi);
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     mmap.clear();
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
+    for (auto mvit = mv.begin(); mvit != mv.end(); ++mvit) {
       mmap[mvit->second] = mvit->first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[21], mmap[13])->getBondType() ==
@@ -4565,8 +4558,7 @@ void testBasicCanon() {
     TEST_ASSERT(SubstructMatch(*m, *m2, mv));
     std::map<int, int> mmap;
     mmap.clear();
-    for (MatchVectType::const_iterator mvit = mv.begin(); mvit != mv.end();
-         ++mvit) {
+    for (auto mvit = mv.begin(); mvit != mv.end(); ++mvit) {
       mmap[mvit->second] = mvit->first;
     }
     TEST_ASSERT(m2->getBondBetweenAtoms(mmap[1], mmap[2])->getBondType() ==
@@ -5684,8 +5676,7 @@ void testGithubIssue539() {
     smilesVec.emplace_back("C1=CC=C[CH+]C=C1");
     smilesVec.emplace_back("c1c[cH+]1");
     smilesVec.emplace_back("c1ccc[cH+]cc1");
-    for (std::vector<std::string>::const_iterator smiles = smilesVec.begin();
-         smiles != smilesVec.end(); ++smiles) {
+    for (auto smiles = smilesVec.begin(); smiles != smilesVec.end(); ++smiles) {
       RWMol *m = SmilesToMol(*smiles);
       TEST_ASSERT(m);
       bool allConjugated = true;
@@ -7218,7 +7209,7 @@ void testGithubIssue868() {
     sstrm.str("");
     TEST_ASSERT(sstrm.str() == "");
     RWMol m;
-    QueryAtom *qa = new QueryAtom();
+    auto *qa = new QueryAtom();
     qa->setQuery(makeAtomTypeQuery(1, aromatic));
     qa->expandQuery(makeAtomNumQuery(6),
                     Queries::CompositeQueryType::COMPOSITE_OR);
