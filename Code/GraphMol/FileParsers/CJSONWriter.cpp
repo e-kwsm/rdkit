146864a663e0e2cf5b852dba332151e6f8816138
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-06-03 01:56:42 +0900

    Code/GraphMol/FileParsers/CJson.cpp

--- Code/GraphMol/FileParsers/CJSONWriter.cpp
+++ Code/GraphMol/FileParsers/CJSONWriter.cpp
@@ -13,21 +13,42 @@
 #include <GraphMol/FileParsers/MolFileStereochem.h>
 #include <fstream>
 #include <sstream>
-#include <boost/format.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
+#include <boost/json/src.hpp>
 #include <RDGeneral/Invariant.h>
 
 namespace RDKit {
 void MolToCJSONBlock(std::ostream &os, const ROMol &mol, int confId,
-                   bool kekulize) {
-  auto pt = molToPTree(mol, confId, kekulize);
-  if (pt.empty()) {
-    return;
+                     bool kekulize) {
+  const Conformer *conf =
+      mol.getNumConformers() ? conf = &mol.getConformer(confId) : nullptr;
+  const bool is3D = conf && conf->is3D();
+
+  const auto nAtoms = mol.getNumAtoms();
+
+  boost::json::array coords_3d;
+  boost::json::array elements_number;
+  boost::json::array formalCharges;
+  if (is3D) {
+    coords_3d.reserve(3u * nAtoms);
   }
-  boost::property_tree::write_xml(
-      os, pt,
-      boost::property_tree::xml_writer_make_settings<std::string>(' ', 2));
+  elements_number.reserve(nAtoms);
+  formalCharges.reserve(nAtoms);
+
+  for (auto i = 0u; i < nAtoms; i++) {
+    const auto &atom = mol.getAtomWithIdx(i);
+    elements_number.push_back(atom->getAtomicNum());
+    if (conf) {
+      const auto &pos = conf->getAtomPos(i);
+      coords_3d.push_back(pos.x);
+      coords_3d.push_back(pos.y);
+      coords_3d.push_back(pos.z);
+    }
+  }
+
+  boost::json::object coords;
+  coords["3d"].emplace_array();
+  boost::json::object root;
+  root["atoms"] = nullptr;
 }
 
 std::string MolToCJSONBlock(const ROMol &mol, int confId, bool kekulize) {
@@ -37,8 +58,8 @@ std::string MolToCJSONBlock(const ROMol &mol, int confId, bool kekulize) {
 }
 
 void MolToCJSONFile(const ROMol &mol, const std::string &fName, int confId,
-                  bool kekulize) {
+                    bool kekulize) {
   std::ofstream ofs{fName};
   MolToCJSONBlock(ofs, mol, confId, kekulize);
 }
-}//
+}  // namespace RDKit
