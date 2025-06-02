1eb0c2726db7eb17bbcf754d8bac5842978eccdc
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-06-03 02:11:18 +0900

    JSON/CJson.h

--- JSON/CJson.h
+++ JSON/CJson.h
@@ -3,9 +3,10 @@
 namespace RDKit {
 class ROMol;
 
-void MolToCJSONBlock(std::ostream &os, const ROMol &mol, int confId,
-                     bool kekulize);
-std::string MolToCJSONBlock(const ROMol &mol, int confId=-1, bool kekulize=true);
-void MolToCJSONFile(const ROMol &mol, const std::string &fName, int confId=-1,
-                    bool kekulize=true);
+void MolToCJSONBlock(std::ostream &os, const ROMol &mol, int confId = -1,
+                     bool kekulize = true);
+std::string MolToCJSONBlock(const ROMol &mol, int confId = -1,
+                            bool kekulize = true);
+void MolToCJSONFile(const ROMol &mol, const std::string &fName, int confId = -1,
+                    bool kekulize = true);
 }  // namespace RDKit
