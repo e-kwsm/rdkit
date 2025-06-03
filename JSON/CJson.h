c6178bed4075a31c8a02f1d3dab3ab1dfb8f0471
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-06-03 18:39:29 +0900

    JSON/CJson.h JSON/Makefile

--- JSON/CJson.h
+++ JSON/CJson.h
@@ -3,8 +3,15 @@
 
 namespace RDKit {
 class ROMol;
+enum class CJSONCoords {
+  _3d,            //!< `[x, y, z, x, y, z, …]`
+  _3dFractional,  //!< `[x, y, z, x, y, z, …]`, fractional
+  _3dSets         //!< `[[x, y, z], [x, y, z], …]`
+};
+
 struct RDKIT_FILEPARSERS_EXPORT CJSONWriterParams {
   bool kekulize = true;
+  CJSONCoords coords = CJSONCoords::_3dSets;
 };
 
 void MolToCJSONBlock(std::ostream &os, const ROMol &mol,
