5e84f6dbf35026af5aa192673cbadee95bdcb6ae
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-06-03 18:34:06 +0900

    Code/GraphMol/FileParsers/CJSONWriter.cpp JSON/CJson.h

--- JSON/CJson.h
+++ JSON/CJson.h
@@ -1,12 +1,16 @@
 #include <iosfwd>
+#include <RDGeneral/export.h>
 
 namespace RDKit {
 class ROMol;
+struct RDKIT_FILEPARSERS_EXPORT CJSONWriterParams {
+  bool kekulize = true;
+};
 
-void MolToCJSONBlock(std::ostream &os, const ROMol &mol, int confId = -1,
-                     bool kekulize = true);
-std::string MolToCJSONBlock(const ROMol &mol, int confId = -1,
-                            bool kekulize = true);
-void MolToCJSONFile(const ROMol &mol, const std::string &fName, int confId = -1,
-                    bool kekulize = true);
+void MolToCJSONBlock(std::ostream &os, const ROMol &mol,
+                     const CJSONWriterParams &params, int confId = -1);
+std::string MolToCJSONBlock(const ROMol &mol, const CJSONWriterParams &params,
+                            int confId = -1);
+void MolToCJSONFile(const ROMol &mol, const std::string &fName,
+                    const CJSONWriterParams &params, int confId = -1);
 }  // namespace RDKit
