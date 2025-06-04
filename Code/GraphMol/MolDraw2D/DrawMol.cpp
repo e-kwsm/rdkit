8661452a2a9f50c31c9467ae21ec4d41b1aa6c7c
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-06-05 06:35:13 +0900

    Code/GraphMol/FileParsers/CMLWriter.cpp Code/GraphMol/MolDraw2D/DrawMol.cpp

--- Code/GraphMol/MolDraw2D/DrawMol.cpp
+++ Code/GraphMol/MolDraw2D/DrawMol.cpp
@@ -1314,7 +1314,7 @@ void DrawMol::drawRadicals(MolDraw2D &drawer) const {
           ncds.x = cds.x + 0.6 * width - spot_rad;
         }
         draw_spot(ncds);
-        /* fallthrough */
+        [[fallthrough]];
       case 1:
         draw_spot(cds);
         break;
@@ -1331,7 +1331,7 @@ void DrawMol::drawRadicals(MolDraw2D &drawer) const {
           ncds.x = cds.x - 6.0 * spot_rad;
         }
         draw_spot(ncds);
-        /* fallthrough */
+        [[fallthrough]];
       case 2:
         if (dir) {
           ncds.y = cds.y + 2.0 * spot_rad;
