1967653638f65424c8d4592d2eb0d9d374a2e3d4
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-06-04 22:02:42 +0900

    Code/GraphMol/FileParsers/CJSONReader.cpp

--- Code/GraphMol/FileParsers/CJSONReader.cpp
+++ Code/GraphMol/FileParsers/CJSONReader.cpp
@@ -1,7 +1,25 @@
 #include "cjson.h"
+#include <GraphMol/RWMol.h>
 
 namespace RDKit {
 namespace v2 {
-namespace FileParsers {}
+namespace FileParsers {
+std::unique_ptr<RWMol> MolFromCJSONDataStream(std::istream &inStream,
+                                              unsigned int &line,
+                                              const CJSONParserParams &params) {
+  return nullptr;
+}
+
+std::unique_ptr<RWMol> MolFromCJSONBlock(const std::string &molBlock,
+                                         const CJSONParserParams &params) {
+  auto rwmol = std::make_unique<RWMol>();
+  return rwmol;
+}
+
+std::unique_ptr<RWMol> MolFromCJSONFile(const std::string &fName,
+                                        const CJSONParserParams &params) {
+  return nullptr;
+}
+}  // namespace FileParsers
 }  // namespace v2
 }  // namespace RDKit
