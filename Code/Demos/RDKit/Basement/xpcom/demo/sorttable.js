6007909d54dcba3202b89c3def9355d7dfb94253
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-11 15:23:51 +0900

    Code/Demos/RDKit/Basement/xpcom/demo/sorttable.js

--- Code/Demos/RDKit/Basement/xpcom/demo/sorttable.js
+++ Code/Demos/RDKit/Basement/xpcom/demo/sorttable.js
@@ -95,7 +95,7 @@ function ts_resortTable(lnk) {
   var sortfn = ts_sort_caseinsensitive;
   if (itm.match(/^\d\d[\/-]\d\d[\/-]\d\d\d\d$/)) sortfn = ts_sort_date;
   if (itm.match(/^\d\d[\/-]\d\d[\/-]\d\d$/)) sortfn = ts_sort_date;
-  if (itm.match(/^[£$]/)) sortfn = ts_sort_currency;
+  if (itm.match(/^[^$]/)) sortfn = ts_sort_currency;
   if (itm.match(/^[-]?[\d\.]+$/)) sortfn = ts_sort_numeric;
   SORT_COLUMN_INDEX = column;
 
