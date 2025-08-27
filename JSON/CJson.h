6487e92d77f79cd4606ec58e2766f4f9b9a0a8e8
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-27 13:26:41 +0900

    Code/GraphMol/FileParsers/CJSONWriter.cpp JSON/CJson.h

--- JSON/CJson.h
+++ JSON/CJson.h
@@ -4,15 +4,8 @@
 namespace RDKit {
 class ROMol;
 
-//! specify format of `coords`
-enum class CJSONCoords {
-  _3d,            //!< `[x, y, z, x, y, z, …]` in Å
-  _3dFractional,  //!< `[x, y, z, x, y, z, …]`, fractional
-};
-
 struct RDKIT_FILEPARSERS_EXPORT CJSONWriterParams {
   bool kekulize = true;
-  CJSONCoords coords = CJSONCoords::_3d;
 };
 
 void MolToCJSONBlock(std::ostream &os, const ROMol &mol,
