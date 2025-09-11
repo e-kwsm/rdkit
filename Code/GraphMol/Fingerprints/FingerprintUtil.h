9ec893dca0123b924aafb523777fefb76f8db0eb
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-12 05:41:02 +0900

    Code/GraphMol/Fingerprints/AtomPairs.cpp Code/GraphMol/Fingerprints/FingerprintUtil.h

--- Code/GraphMol/Fingerprints/FingerprintUtil.h
+++ Code/GraphMol/Fingerprints/FingerprintUtil.h
@@ -26,7 +26,7 @@
 namespace RDKit {
 namespace AtomPairs {
 const unsigned int numTypeBits = 4;
-const unsigned int atomNumberTypes[1 << numTypeBits] = {
+const std::array<unsigned int, 1 << numTypeBits> atomNumberTypes = {
     5, 6, 7, 8, 9, 14, 15, 16, 17, 33, 34, 35, 51, 52, 53};
 const unsigned int numPiBits = 2;
 const unsigned int maxNumPi = (1 << numPiBits) - 1;
