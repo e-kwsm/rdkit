2a6425930ab6e64b82b09d056131f4d39fc44316
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-10-03 04:30:39 +0900

    Code/GraphMol/Descriptors/catch_tests.cpp Code/MinimalLib/common_defs.h

--- Code/GraphMol/Descriptors/catch_tests.cpp
+++ Code/GraphMol/Descriptors/catch_tests.cpp
@@ -558,7 +558,7 @@ TEST_CASE("DCLV") {
       pathName + "/Code/GraphMol/Descriptors/test_data/1mup.pdb";
   auto m = v2::FileParsers::MolFromPDBFile(pdbName);
   std::vector<double> radii;
-  for (const auto atom : m->atoms()) {
+  for (auto *const atom : m->atoms()) {
     radii.push_back(tbl->getRvdw(atom->getAtomicNum()));
   }
   REQUIRE(m);
@@ -628,7 +628,7 @@ TEST_CASE("DCLV") {
         pathName + "/Code/GraphMol/Descriptors/test_data/TZL_model.sdf";
     auto m = v2::FileParsers::MolFromMolFile(sdfName);
     std::vector<double> radii;
-    for (const auto atom : m->atoms()) {
+    for (auto *const atom : m->atoms()) {
       radii.push_back(tbl->getRvdw(atom->getAtomicNum()));
     }
     REQUIRE(m);
