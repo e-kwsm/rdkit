3b0f81c36b1906329a0658d37257213a295e882a
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-10-04 16:04:31 +0900

    Code/GraphMol/ChemReactions/Wrap/rdChemReactions.cpp

--- Code/GraphMol/ChemReactions/Wrap/rdChemReactions.cpp
+++ Code/GraphMol/ChemReactions/Wrap/rdChemReactions.cpp
@@ -175,7 +175,7 @@ bool RunReactantInPlace(ChemicalReaction *self, ROMol *reactant,
 
 python::tuple ValidateReaction(const ChemicalReaction *self,
                                bool silent = false) {
-  unsigned int numWarn, numError;
+  unsigned int numWarn = 0, numError = 0;
   self->validate(numWarn, numError, silent);
   return python::make_tuple(numWarn, numError);
 }
@@ -284,16 +284,16 @@ void Compute2DCoordsForReaction(RDKit::ChemicalReaction &rxn,
 
 bool IsMoleculeReactantOfReaction(const ChemicalReaction &rxn,
                                   const ROMol &mol) {
-  unsigned int which;
+  unsigned int which = 0;
   return isMoleculeReactantOfReaction(rxn, mol, which);
 }
 bool IsMoleculeProductOfReaction(const ChemicalReaction &rxn,
                                  const ROMol &mol) {
-  unsigned int which;
+  unsigned int which = 0;
   return isMoleculeProductOfReaction(rxn, mol, which);
 }
 bool IsMoleculeAgentOfReaction(const ChemicalReaction &rxn, const ROMol &mol) {
-  unsigned int which;
+  unsigned int which = 0;
   return isMoleculeAgentOfReaction(rxn, mol, which);
 }
 
@@ -307,7 +307,7 @@ ChemicalReaction *ReactionFromSmarts(const char *smarts, python::dict replDict,
     replacements[python::extract<std::string>(replDict.keys()[i])] =
         python::extract<std::string>(replDict.values()[i]);
   }
-  ChemicalReaction *res;
+  ChemicalReaction *res = nullptr;
   res = RxnSmartsToChemicalReaction(smarts, &replacements, useSmiles);
   return res;
 }
@@ -447,7 +447,7 @@ python::object PreprocessReaction(ChemicalReaction &reaction,
 
   unsigned int nReactants = reaction.getNumReactantTemplates();
   unsigned int nProducts = reaction.getNumProductTemplates();
-  unsigned int nWarn, nError;
+  unsigned int nWarn = 0, nError = 0;
   reaction.validate(nWarn, nError);
   std::vector<std::vector<std::pair<unsigned int, std::string>>> labels;
 
