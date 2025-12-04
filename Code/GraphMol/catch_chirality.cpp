4dd6b9532c5c50aad63a5f3b6502aeebd1ada06e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-12-04 22:30:43 +0900

    Code/GraphMol/catch_chirality.cpp Code/GraphMol/hanoitest.cpp Code/GraphMol/molopstest.cpp Code/GraphMol/testChirality.cpp

--- Code/GraphMol/catch_chirality.cpp
+++ Code/GraphMol/catch_chirality.cpp
@@ -3299,7 +3299,7 @@ TEST_CASE(
   }
 }
 
-void testStereoValidationFromMol(std::string molBlock,
+void testStereoValidationFromMol(const std::string &molBlock,
                                  std::string expectedSmiles, bool legacyFlag,
                                  bool canonicalFlag = false) {
   RDKit::Chirality::setUseLegacyStereoPerception(legacyFlag);
