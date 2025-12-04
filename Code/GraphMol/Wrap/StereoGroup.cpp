feab0869c4c55629a147043b61c4ead72cf449ab
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-12-04 22:58:52 +0900

    Code/GraphMol/Wrap/Conformer.cpp Code/GraphMol/Wrap/ContextManagers.h Code/GraphMol/Wrap/ForwardSDMolSupplier.cpp Code/GraphMol/Wrap/Mol.cpp Code/GraphMol/Wrap/MolOps.cpp Code/GraphMol/Wrap/MultithreadedSDMolSupplier.cpp Code/GraphMol/Wrap/MultithreadedSmilesMolSupplier.cpp Code/GraphMol/Wrap/SDMolSupplier.cpp Code/GraphMol/Wrap/SDWriter.cpp Code/GraphMol/Wrap/SmilesMolSupplier.cpp Code/GraphMol/Wrap/SmilesWriter.cpp Code/GraphMol/Wrap/StereoGroup.cpp Code/GraphMol/Wrap/SubstanceGroup.cpp Code/GraphMol/Wrap/TDTWriter.cpp Code/GraphMol/Wrap/rdmolfiles.cpp Code/GraphMol/Wrap/seqs.hpp

--- Code/GraphMol/Wrap/StereoGroup.cpp
+++ Code/GraphMol/Wrap/StereoGroup.cpp
@@ -28,7 +28,8 @@ std::string stereoGroupClassDoc =
     "is a mix\nof diastereomers.\n";
 
 StereoGroup *createStereoGroup(StereoGroupType typ, ROMol &mol,
-                               python::object atomIds, python::object bondIds, unsigned readId) {
+                               const python::object &atomIds,
+                               const python::object &bondIds, unsigned readId) {
   std::vector<Atom *> cppAtoms;
   std::vector<Bond *> cppBonds;
   python::stl_input_iterator<unsigned int> beg(atomIds), end;
