65931fb436488fab9fe6dd2eed0df27260824ee5
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-12-05 08:40:14 +0900

    Code/ML/Cluster/Murtagh/Clustering.cpp Code/ML/Data/cQuantize.cpp

--- Code/ML/Cluster/Murtagh/Clustering.cpp
+++ Code/ML/Cluster/Murtagh/Clustering.cpp
@@ -59,7 +59,7 @@ static void capsule_cleanup(PyObject *capsule) {
   free(ptr);
 }
 
-static PyObject *Clustering_MurtaghCluster(python::object data, int nPts,
+static PyObject *Clustering_MurtaghCluster(const python::object &data, int nPts,
                                            int sz, int option) {
   PyArrayObject *dataContig;
   boost::int64_t *ia, *ib;
@@ -117,8 +117,8 @@ void distclusterit(real *dists, boost::int64_t n, boost::int64_t iopt,
   distdriver_(&n, &len, dists, &iopt, ia, ib, crit);
 };
 
-static PyObject *Clustering_MurtaghDistCluster(python::object data, int nPts,
-                                               int option) {
+static PyObject *Clustering_MurtaghDistCluster(const python::object &data,
+                                               int nPts, int option) {
   PyArrayObject *dataContig;
   boost::int64_t *ia, *ib;
   real *crit;
