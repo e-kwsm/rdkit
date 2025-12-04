89a3a2e88a344db9296f6ffd6156887a107e0fca
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-12-04 22:30:43 +0900

    Code/GraphMol/catch_chirality.cpp Code/GraphMol/hanoitest.cpp Code/GraphMol/molopstest.cpp Code/GraphMol/testChirality.cpp

--- Code/GraphMol/catch_chirality.cpp
+++ Code/GraphMol/catch_chirality.cpp
@@ -3298,7 +3298,7 @@ TEST_CASE(
   }
 }
 
-void testStereoValidationFromMol(std::string molBlock,
+void testStereoValidationFromMol(const std::string &molBlock,
                                  std::string expectedSmiles, bool legacyFlag,
                                  bool canonicalFlag = false) {
   RDKit::Chirality::setUseLegacyStereoPerception(legacyFlag);
--- Code/GraphMol/hanoitest.cpp
+++ Code/GraphMol/hanoitest.cpp
@@ -844,7 +844,7 @@ ROMol *_renumber(const ROMol *m, std::vector<unsigned int> &nVect,
   return nm;
 }
 
-void _renumberTest(const ROMol *m, std::string inSmiles,
+void _renumberTest(const ROMol *m, const std::string &inSmiles,
                    unsigned int numRenumbers) {
   PRECONDITION(m, "no molecule");
   //    std::cerr<<">>>>>>>>>>>>>>>>>>>>>>>>>>>"<<std::endl;
@@ -883,7 +883,7 @@ void _renumberTest(const ROMol *m, std::string inSmiles,
   }
 }
 
-void _renumberTest2(const ROMol *m, std::string inSmiles,
+void _renumberTest2(const ROMol *m, const std::string &inSmiles,
                     unsigned int numRenumbers) {
   PRECONDITION(m, "no molecule");
 
