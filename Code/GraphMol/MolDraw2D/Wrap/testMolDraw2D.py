b1759687fc918ae1f3af36d9cf4d32ced7f27f82
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-12 16:23:11 +0900

    assertNotEqual

--- Code/GraphMol/MolDraw2D/Wrap/testMolDraw2D.py
+++ Code/GraphMol/MolDraw2D/Wrap/testMolDraw2D.py
@@ -24,8 +24,8 @@ class TestCase(unittest.TestCase):
     d.DrawMolecule(m)
     d.FinishDrawing()
     txt = d.GetDrawingText()
-    self.assertTrue(txt.find("<svg") != -1)
-    self.assertTrue(txt.find("</svg>") != -1)
+    self.assertNotEqual(txt.find("<svg"), -1)
+    self.assertNotEqual(txt.find("</svg>"), -1)
 
   def test2(self):
     m = Chem.MolFromSmiles('c1ccc(C)c(C)c1C')
@@ -36,10 +36,10 @@ class TestCase(unittest.TestCase):
     d.DrawMolecule(m)
     d.FinishDrawing()
     txt = d.GetDrawingText()
-    self.assertTrue(txt.find(">f</text>") != -1)
-    self.assertTrue(txt.find(">o</text>") != -1)
-    self.assertTrue(txt.find(">l</text>") != -1)
-    self.assertTrue(txt.find(">a</text>") != -1)
+    self.assertNotEqual(txt.find(">f</text>"), -1)
+    self.assertNotEqual(txt.find(">o</text>"), -1)
+    self.assertNotEqual(txt.find(">l</text>"), -1)
+    self.assertNotEqual(txt.find(">a</text>"), -1)
 
   @unittest.skipUnless(hasattr(rdMolDraw2D, 'MolDraw2DCairo'), 'Cairo support not enabled')
   def testGithubIssue571(self):
@@ -216,8 +216,8 @@ M  END""")
     d.DrawReaction(rxn)
     d.FinishDrawing()
     txt = d.GetDrawingText()
-    self.assertTrue(txt.find("<svg") != -1)
-    self.assertTrue(txt.find("</svg>") != -1)
+    self.assertNotEqual(txt.find("<svg"), -1)
+    self.assertNotEqual(txt.find("</svg>"), -1)
     # print(txt,file=open('blah1.svg','w+'))
 
   def testReaction2(self):
@@ -228,8 +228,8 @@ M  END""")
     d.DrawReaction(rxn, highlightByReactant=True)
     d.FinishDrawing()
     txt = d.GetDrawingText()
-    self.assertTrue(txt.find("<svg") != -1)
-    self.assertTrue(txt.find("</svg>") != -1)
+    self.assertNotEqual(txt.find("<svg"), -1)
+    self.assertNotEqual(txt.find("</svg>"), -1)
     # print(txt,file=open('blah2.svg','w+'))
 
   def testReaction3(self):
@@ -241,8 +241,8 @@ M  END""")
     d.DrawReaction(rxn, highlightByReactant=True, highlightColorsReactants=colors)
     d.FinishDrawing()
     txt = d.GetDrawingText()
-    self.assertTrue(txt.find("<svg") != -1)
-    self.assertTrue(txt.find("</svg>") != -1)
+    self.assertNotEqual(txt.find("<svg"), -1)
+    self.assertNotEqual(txt.find("</svg>"), -1)
 
   def testReaction4(self):
     rxn = rdChemReactions.ReactionFromSmarts(
